f6949fbbfb4c5ce9c7fecbcaa49bc37901795291
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2024-07-17 10:11:04 +0700

    update

--- src/components/layout/content.js
+++ src/components/layout/content.js
@@ -1,23 +1,56 @@
 import React from 'react';
 
-const ContentPage  = () => {
+const ContentPage = () => {
     return (
-        <div className="bg-gray-100 h-screen">
-           <div>
+        <div className="bg-gray-100">
+            <div className="w-full h-[500px] bg-green-200">
 
-           </div>
+            </div>
 
-           <div>
+            <div className="w-full h-[560px] border-b-2 border-gray-300">
+                <div className="w-full h-[60px] bg-white">
 
-           </div>
+                </div>
+                <div className="w-full h-[500px] ">
+                    hhhh
+                </div>
+            </div>
 
-           <div>
+            <div className="w-full h-[560px] border-b-2 border-gray-300">
+                <div className="w-full h-[60px] bg-white">
 
-           </div>
+                </div>
+                <div className="w-full h-[500px] ">
+                    hhhh
+                </div>
+            </div>
 
-           <div>
-            
-           </div>
+            <div className="w-full h-[560px] border-b-2 border-gray-300">
+                <div className="w-full h-[60px] bg-white">
+
+                </div>
+                <div className="w-full h-[500px] ">
+                    hhhh
+                </div>
+            </div>
+
+            <div className="w-full h-[560px] border-b-2 border-gray-300">
+                <div className="w-full h-[60px] bg-white">
+
+                </div>
+                <div className="w-full h-[500px] ">
+                    hhhh
+                </div>
+            </div>
+
+            <div className="w-full h-[560px] border-b-2 border-gray-300">
+                <div className="w-full h-[60px] bg-white">
+
+                </div>
+                <div className="w-full h-[500px] ">
+                    hhhh
+                </div>
+            </div>
         </div>
     );
 }
