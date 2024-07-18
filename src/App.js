803f05002d7ffe6a7e46a2a803a3bceb4f27124f
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2024-07-18 12:19:07 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { useEffect } from 'react';
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate  } from 'react-router-dom';
 
 import HomePage from './components/layout/index';
 
-//import './App.css';
+import OriginalsPage from './components/categories/originals/originals'
+import VideosPage from './components/categories/videos/videos'
+import GenresPage from './components/categories/genres/genres'
+import PopularPage from './components/categories/popular/popular'
+
+import NotFoundPage from './components/layout/notFoundPage';
+
+import './App.css';
 
 function App() {
 
@@ -16,7 +23,16 @@ function App() {
     <BrowserRouter>
       <Routes>
 
-      <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<HomePage />} />
+
+        <Route path="/originals" element={<OriginalsPage />} />
+        <Route path="/videos" element={<VideosPage />} />
+        <Route path="/genres" element={<GenresPage />} />
+        <Route path="/popular" element={<PopularPage />} />
+
+        {/* Xử lý trang lỗi */}
+        <Route path="*" element={<Navigate to="/404" />} />
+        <Route path="/404" element={<NotFoundPage />} />
 
       </Routes>
     </BrowserRouter>
---
+++ src/components/layout/notFoundPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFoundPage  = () => {
+    return (
+        <div className="text-center mt-[200px]">
+      <h1>Trang không tồn tại</h1>
+      {/* Bạn có thể tùy chỉnh nội dung và giao diện của trang lỗi ở đây */}
+    </div>
+    );
+}
+
+export default NotFoundPage;
