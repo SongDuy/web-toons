6ee6e96dbb22369151af40024fd5181dbbdd1c27
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2024-07-22 21:24:29 +0700

    update

--- src/App.js
+++ src/App.js
@@ -7,13 +7,14 @@ import HomePage from './components/layout/index';
 import OriginalsPage from './components/categories/originals/originals'
 import OriginalSeriesPage from './components/categories/originals/originalSeries';
 import DisplayOriginalPage from './components/categories/originals/displayOriginal';
-
 import VideosPage from './components/categories/videos/videos'
 import VideoSeriesPage from './components/categories/videos/videoSeries';
 import DisplayVideoPage from './components/categories/videos/displayVideo';
-
 import GenresPage from './components/categories/genres/genres'
 import PopularPage from './components/categories/popular/popular'
+
+import CreateOriginalPage from './components/create/createOriginal';
+import CreateVideoPage from './components/create/createVideo';
 
 import NotFoundPage from './components/layout/notFoundPage';
 
@@ -42,6 +43,9 @@ function App() {
         <Route path="/genres" element={<GenresPage />} />
 
         <Route path="/popular" element={<PopularPage />} />
+
+        <Route path="/creact/original" element={<CreateOriginalPage />} />
+        <Route path="/cearct/video" element={<CreateVideoPage />} />
 
         {/* Xử lý trang lỗi */}
         <Route path="*" element={<Navigate to="/404" />} />
---
+++ src/components/create/createVideo.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import HeaderPage from '../layout/header';
+import FooterPage from '../layout/footer';
+
+const CreateVideoPage = () => {
+    return (
+        <div>
+            <HeaderPage />
+
+            <div className="w-full h-[300px] bg-gray-100">
+
+            </div>
+
+            <FooterPage />
+        </div>
+
+    );
+}
+
+export default CreateVideoPage;
