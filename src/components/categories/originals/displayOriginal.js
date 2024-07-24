9041be0de9fc6d53af15c58beea6616eb27b1bc8
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2024-07-24 12:00:08 +0700

    update

--- src/components/categories/originals/displayOriginal.js
+++ src/components/categories/originals/displayOriginal.js
@@ -1,28 +1,45 @@
 import React from 'react';
 
+import FooterPage from '../../layout/footer';
+
 const DisplayOriginalPage = () => {
     return (
-        <div className="w-full h-full bg-white">
-            {/* Thanh công cụ */}
-            <div className="">
+        <div>
+            <div className="w-full h-full bg-white">
+                {/* Thanh công cụ */}
+                <div className="w-full h-[50px] bg-black">
+                    <ul>
+                        <li className="">
 
+                        </li>
+                        <li className="">
+
+                        </li>
+                        <li className="">
+
+                        </li>
+                    </ul>
+                </div>
+
+                {/* Hiển thị nội dung truyện */}
+                <div className="w-full h-[1000px] bg-white">
+
+                </div>
+
+                {/* Hiển thị yêu thích theo dõi */}
+                <div className="w-full h-[250px] bg-green-200">
+
+                </div>
+
+                {/* Hiển thị bình luận và danh sách truyện nổi bật */}
+                <div className="w-full h-[500px] bg-white">
+
+                </div>
             </div>
 
-            {/* Hiển thị nội dung truyện */}
-            <div className="">
-
-            </div>
-
-            {/* Hiển thị yêu thích theo dõi */}
-            <div className="">
-
-            </div>
-
-            {/* Hiển thị bình luận và danh sách truyện nổi bật */}
-            <div className="">
-
-            </div>
+            <FooterPage />
         </div>
+
     );
 }
 
