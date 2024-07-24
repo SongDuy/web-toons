1ffd1dca9b46809038a5316dd2f1fe3a55ace2df
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2024-07-24 12:26:42 +0700

    update

--- src/components/categories/originals/displayOriginal.js
+++ src/components/categories/originals/displayOriginal.js
@@ -7,17 +7,28 @@ const DisplayOriginalPage = () => {
         <div>
             <div className="w-full h-full bg-white">
                 {/* Thanh công cụ */}
-                <div className="w-full h-[50px] bg-black">
-                    <ul>
+                <div className="w-full h-[50px] px-5 bg-black flex items-center">
+                    <ul className="w-full flex">
+
+                        <li className="mr-auto">
+                            <div className="">
+                                <span className="text-white">Logo</span>
+                            </div>
+
+                            <div className="">
+
+                            </div>
+                        </li>
+
                         <li className="">
+                            <span className="text-white">kk</span>
+                        </li>
+
+                        <li className="bg-white ml-auto">
+                            <span className="ml-auto">jjjjjjjjjjk k k kk k k k k jjjjj</span>
 
                         </li>
-                        <li className="">
 
-                        </li>
-                        <li className="">
-
-                        </li>
                     </ul>
                 </div>
 
