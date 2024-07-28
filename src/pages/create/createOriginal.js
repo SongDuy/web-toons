851585a3a0e404564d74a7631b774c88d44aefaa
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2024-07-28 16:01:12 +0700

    update new

--- src/pages/create/createOriginal.js
+++ src/pages/create/createOriginal.js
@@ -38,8 +38,94 @@ const CreateOriginalPage = () => {
                     </ul>
                 </div>
 
-            </div>
+                <div className="w-full h-full px-[200px]">
+                    <div className="w-full h-full py-5 flex">
+                        <div className="w-3/12 h-full">
 
+                            <div className="w-[220px] h-full">
+                                <div className="w-full py-3">
+                                    <span className="w-full font-semibold text-xl">
+                                        Square Thumbnail
+                                    </span>
+                                </div>
+
+                                <div className="w-full h-[220px] shadow-md bg-red-100 rounded border hover:border-green-500 hover:text-gray-500 flex items-center justify-center">
+                                    kkk
+                                </div>
+
+                                <div className="w-full py-3">
+                                    <span className="block w-full font-semibold text-sm text-gray-500">
+                                        Image size must be 1080x1080.
+                                    </span>
+                                    <span className="block w-full font-semibold text-sm text-gray-500">
+                                        Image must be less than 500KB.
+                                    </span>
+
+                                    <span className="block w-full font-semibold text-sm text-gray-500">
+                                        Only JPG, JPEG, and PNG formats are allowed.
+                                    </span>
+
+
+                                </div>
+                            </div>
+
+                            <div className="w-[220px] h-full">
+                                <div className="w-full py-3">
+                                    <span className="w-full font-semibold text-xl">
+                                        Vertical Thumbnail
+                                    </span>
+                                </div>
+
+                                <div className="w-full h-[220px] shadow-md bg-red-100 rounded border hover:border-green-500 hover:text-gray-500 flex items-center justify-center">
+                                    kkk
+                                </div>
+
+                                <div className="w-[230px] py-3">
+                                    <span className="block w-full font-semibold text-sm text-gray-500">Image size must be 1080x1920.</span>
+                                    <span className="block w-full font-semibold text-sm text-gray-500">Image must be less than 700KB.</span>
+                                    <span className="block w-full font-semibold text-sm text-gray-500">Only JPG, JPEG, and PNG formats are allowed.</span>
+                                </div>
+
+
+                            </div>
+                        </div>
+
+                        <div className="w-9/12 h-full bg-blue-200 px-5">
+
+                            <div className="w-full py-3 flex ">
+                                <span className="w-full font-semibold text-xl">
+                                    Genre
+                                </span>
+
+                                <span className="w-full font-semibold text-xl">
+                                    Age
+                                </span>
+                            </div>
+
+
+                            <div className="">
+
+                            </div>
+
+                            <div className="">
+
+                            </div>
+
+                            <div className="">
+
+                            </div>
+
+                            <div className="">
+
+                            </div>
+
+
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
         </div >
 
     );
