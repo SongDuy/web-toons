e87345dbc54e6972b57f86be5ea04046b71d9e5f
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2024-08-10 22:09:38 +0700

    update new

--- src/components/layout/layoutUser/search.js
+++ src/components/layout/layoutUser/search.js
@@ -60,7 +60,7 @@ const SearchPage = ({ closeModal }) => {
                         {dataAlsoLike.map(item => (
                             <Link to={`/original/series`}>
                                 <li
-                                    className="w-[290px] h-[80px] flex bg-red-50 rounded-xl shadow-md cursor-pointer hover:bg-red-100"
+                                    className="w-[290px] h-[80px] flex rounded-xl cursor-pointer"
                                     key={item.id}
                                 >
 
@@ -73,7 +73,7 @@ const SearchPage = ({ closeModal }) => {
                                     </div>
 
                                     <div className="h-full rounded-xl px-3 py-3">
-                                        <div className="w-[180px] h-[75px] overflow-hidden">
+                                        <div className="w-[180px] h-[50px] overflow-hidden">
                                             <span className="w-full text-lg font-semibold leading-[1.2] line-clamp-2">
                                                 {item.name}
                                             </span>
