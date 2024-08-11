b40346ed009e7e3e1f2c0ea7616772c88522ea95
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2024-08-11 22:36:59 +0700

    update new

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -12,22 +12,22 @@ const LoginPage = ({ closeModal }) => {
     return (
         <div className="w-screen h-screen bg-black bg-opacity-50 flex items-center justify-center fixed inset-0 z-50" onClick={handleBackdropClick}> {/* backdrop-blur-sm */}
 
-            <div className="w-[350px] h-[500px] px-5 py-5 bg-white shadow rounded-lg flex justify-center">
+            <div className="w-[350px] h-[500px] px-5 py-5 bg-white shadow rounded-lg">
                 <div>
-                    <span className="text-[25px] font-semibold">Log In Now</span>
+                    <span className="text-[25px] flex justify-center font-semibold">Log In Now</span>
 
-                    <ul className="w-full h-[50px] bg-red-100">
+                    <ul className="w-full h-full mt-10 bg-red-100 grid grid-cols-1 gap-y-5">
 
-                        <li className="w-full h-[50px] bg-red-100">
-
+                        <li className="h-[50px] border bg-red-100">
+                            k,kkk
                         </li>
 
-                        <li className="">
-
+                        <li className="h-[50px] bg-red-100">
+                            kkk
                         </li>
 
-                        <li className="">
-
+                        <li className="h-[50px] bg-red-100">
+                            kkk
                         </li>
 
                     </ul>
