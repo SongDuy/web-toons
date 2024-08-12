d30aaaea8c8d743e6b29c4a33eb5c398a48e98e8
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2024-08-12 22:05:05 +0700

    update new

--- src/components/Home/childContent/weekdayOriginalsAndVideos.js
+++ src/components/Home/childContent/weekdayOriginalsAndVideos.js
@@ -141,10 +141,10 @@ const WeekdayOriginalsAndVideosPage = () => {
 
                                 <div className="w-full absolute inset-0 flex flex-wrap items-center px-3 py-3">
                                     <div className="w-full h-[65px] mb-auto overflow-hidden">
-                                        <span className="text-black text-lg font-semibold leading-[1.2] line-clamp-2">
+                                        <span className="text-gray-800 text-opacity-80 text-lg font-semibold leading-[1.2] line-clamp-2">
                                             {item.name}
                                         </span>
-                                        <span className="text-black text-md leading-[1.2] line-clamp-1">
+                                        <span className="text-gray-500 text-opacity-80 text-md leading-[1.2] line-clamp-1">
                                             {item.auth}
                                         </span>
                                     </div>
