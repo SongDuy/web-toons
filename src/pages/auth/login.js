b58c5032296fde801a69959dc8e53f6ae556057f
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2024-08-12 19:58:47 +0700

    update new

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import GoogleIcon from '@mui/icons-material/Google';
+import EmailIcon from '@mui/icons-material/Email';
 import '../../App.css';
 
 const LoginPage = ({ closeModal }) => {
@@ -16,18 +19,36 @@ const LoginPage = ({ closeModal }) => {
                 <div>
                     <span className="text-[25px] flex justify-center font-semibold">Log In Now</span>
 
-                    <ul className="w-full h-full mt-10 bg-red-100 grid grid-cols-1 gap-y-5">
+                    <ul className="w-full h-full mt-10 grid grid-cols-1 gap-y-5">
 
-                        <li className="h-[50px] border bg-red-100">
-                            k,kkk
+                        <li className="w-full h-[50px] cursor-pointer px-5 py-2 bg-red-50 hover:bg-red-100 shadow-md flex items-center justify-center rounded">
+                            <span className="mr-auto">
+                                <EmailIcon />
+                            </span>
+
+                            <span className="mr-auto font-semibold">
+                                Continue with Email
+                            </span>
                         </li>
 
-                        <li className="h-[50px] bg-red-100">
-                            kkk
+                        <li className="w-full h-[50px] cursor-pointer px-5 py-2 bg-red-50 hover:bg-red-100 shadow-md flex items-center justify-center rounded">
+                            <span className="mr-auto">
+                                <GoogleIcon />
+                            </span>
+
+                            <span className="mr-auto font-semibold">
+                                Continue with Google
+                            </span>
                         </li>
 
-                        <li className="h-[50px] bg-red-100">
-                            kkk
+                        <li className="w-full h-[50px] cursor-pointer px-5 py-2 bg-red-50 hover:bg-red-100 shadow-md flex items-center justify-center rounded">
+                            <span className="mr-auto">
+                                <FacebookIcon />
+                            </span>
+
+                            <span className="mr-auto font-semibold">
+                                Continue with Facebook
+                            </span>
                         </li>
 
                     </ul>
