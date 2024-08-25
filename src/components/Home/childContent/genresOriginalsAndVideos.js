7cd4d790fe614349c55398d500cc3047afe6c650
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2024-08-25 11:28:52 +0700

    update new

--- src/components/Home/childContent/genresOriginalsAndVideos.js
+++ src/components/Home/childContent/genresOriginalsAndVideos.js
@@ -220,8 +220,19 @@ const GenresOriginalsAndVideosPage = () => {
                                             />
 
                                             {hoveredOriginalItem === item.id && (
-                                                <div className="absolute inset-0 bg-black bg-opacity-30 border-4 border-yellow-500 rounded-md flex items-center justify-center text-yellow-500 z-10">
-                                                    <AutoStoriesIcon sx={{ fontSize: 40 }} />
+                                                // <div className="absolute inset-0 bg-black bg-opacity-30 border-4 border-yellow-500 rounded-md flex items-center justify-center text-yellow-500 z-10">
+                                                //     <AutoStoriesIcon sx={{ fontSize: 40 }} />
+                                                // </div>
+
+                                                <div class="absolute inset-0  flex items-center justify-center text-yellow-500 z-10">
+                                                    <img
+                                                        src={item.img}
+                                                        alt="img"
+                                                        class="object-fill w-full h-full rounded-md"
+                                                    />
+                                                    <div className="absolute w-full h-full flex items-center justify-center border-4 border-yellow-500 rounded-md">
+                                                        <AutoStoriesIcon sx={{ fontSize: 40 }} />
+                                                    </div>
                                                 </div>
                                             )}
                                         </div>
@@ -303,8 +314,19 @@ const GenresOriginalsAndVideosPage = () => {
                                             />
 
                                             {hoveredVideoItem === item.id && (
-                                                <div className="absolute inset-0 bg-black bg-opacity-30 border-4 border-yellow-500 rounded-md flex items-center justify-center text-yellow-500 z-10">
-                                                    <PlayArrowIcon sx={{ fontSize: 60 }} />
+                                                // <div className="absolute inset-0 bg-black bg-opacity-30 border-4 border-yellow-500 rounded-md flex items-center justify-center text-yellow-500 z-10">
+                                                //     <PlayArrowIcon sx={{ fontSize: 60 }} />
+                                                // </div>
+
+                                                <div class="absolute inset-0  flex items-center justify-center text-yellow-500 z-10">
+                                                    <img
+                                                        src={item.img}
+                                                        alt="img"
+                                                        class="object-fill w-full h-full rounded-md"
+                                                    />
+                                                    <div className="absolute w-full h-full flex items-center justify-center border-4 border-yellow-500 rounded-md">
+                                                        <PlayArrowIcon sx={{ fontSize: 60 }} />
+                                                    </div>
                                                 </div>
                                             )}
                                         </div>
