af88573b8ffde494c401f38a6f2fce33d88e3dec
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2024-08-26 09:55:32 +0700

    update new

--- src/pages/admin/admin.js
+++ src/pages/admin/admin.js
@@ -1,5 +1,9 @@
 import React from 'react';
-//import '../../App.css'
+
+import PieChartIcon from '@mui/icons-material/PieChart';
+import PersonIcon from '@mui/icons-material/Person';
+import CollectionsIcon from '@mui/icons-material/Collections';
+
 const AdminPage = () => {
     return (
         <div className="w-full h-full border">
@@ -13,42 +17,84 @@ const AdminPage = () => {
                     </div>
 
                     <ul className="mt-10 grid grid-cols-1 gap-4">
-                        <li className="w-full h-[50px] flex items-center justify-center bg-white rounded shadow hover:text-blue-500 hover:bg-yellow-100 cursor-pointer">
-                            <span className="font-semibold">
+                        <li className="w-full h-[50px] cursor-pointer px-5 py-2 bg-white hover:bg-red-100 shadow-md flex items-center justify-center rounded">
+                            <span className="mr-auto">
+                                <PieChartIcon />
+                            </span>
+
+                            <span className="mr-auto font-semibold">
                                 Dashboard
                             </span>
                         </li>
 
-                        <li className="w-full h-[50px] flex items-center justify-center bg-white rounded shadow hover:text-blue-500 hover:bg-yellow-100 cursor-pointer">
-                            <span className="font-semibold">
+                        <li className="w-full h-[50px] cursor-pointer px-5 py-2 bg-white hover:bg-red-100 shadow-md flex items-center justify-center rounded">
+                            <span className="mr-auto">
+                                <PersonIcon />
+                            </span>
+
+                            <span className="mr-auto font-semibold">
                                 Users
                             </span>
                         </li>
 
-                        <li className="w-full h-[50px] flex items-center justify-center bg-white rounded shadow hover:text-blue-500 hover:bg-yellow-100 cursor-pointer">
-                            <span className="font-semibold">
+                        <li className="w-full h-[50px] cursor-pointer px-5 py-2 bg-white hover:bg-red-100 shadow-md flex items-center justify-center rounded">
+                            <span className="mr-auto">
+                                <CollectionsIcon />
+                            </span>
+
+                            <span className="mr-auto font-semibold">
                                 Banner
                             </span>
                         </li>
 
-                        <li className="w-full h-[50px] flex items-center justify-center bg-white rounded shadow hover:bg-yellow-100 cursor-pointer">
+                        <li className="w-full h-[50px] cursor-pointer px-5 py-2 bg-white hover:bg-red-100 shadow-md flex items-center justify-center rounded">
+                            <span className="mr-auto">
+                                <CollectionsIcon />
+                            </span>
 
+                            <span className="mr-auto font-semibold">
+                                Banner
+                            </span>
                         </li>
 
-                        <li className="w-full h-[50px] flex items-center justify-center bg-white rounded shadow hover:bg-yellow-100 cursor-pointer">
+                        <li className="w-full h-[50px] cursor-pointer px-5 py-2 bg-white hover:bg-red-100 shadow-md flex items-center justify-center rounded">
+                            <span className="mr-auto">
+                                <CollectionsIcon />
+                            </span>
 
+                            <span className="mr-auto font-semibold">
+                                Banner
+                            </span>
                         </li>
 
-                        <li className="w-full h-[50px] flex items-center justify-center bg-white rounded shadow hover:bg-yellow-100 cursor-pointer">
+                        <li className="w-full h-[50px] cursor-pointer px-5 py-2 bg-white hover:bg-red-100 shadow-md flex items-center justify-center rounded">
+                            <span className="mr-auto">
+                                <CollectionsIcon />
+                            </span>
 
+                            <span className="mr-auto font-semibold">
+                                Banner
+                            </span>
                         </li>
 
-                        <li className="w-full h-[50px] flex items-center justify-center bg-white rounded shadow hover:bg-yellow-100 cursor-pointer">
+                        <li className="w-full h-[50px] cursor-pointer px-5 py-2 bg-white hover:bg-red-100 shadow-md flex items-center justify-center rounded">
+                            <span className="mr-auto">
+                                <CollectionsIcon />
+                            </span>
 
+                            <span className="mr-auto font-semibold">
+                                Banner
+                            </span>
                         </li>
 
-                        <li className="w-full h-[50px] flex items-center justify-center bg-white rounded shadow hover:bg-yellow-100 cursor-pointer">
+                        <li className="w-full h-[50px] cursor-pointer px-5 py-2 bg-white hover:bg-red-100 shadow-md flex items-center justify-center rounded">
+                            <span className="mr-auto">
+                                <CollectionsIcon />
+                            </span>
 
+                            <span className="mr-auto font-semibold">
+                                Banner
+                            </span>
                         </li>
                     </ul>
                 </div>
