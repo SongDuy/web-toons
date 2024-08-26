0cba5c52cf06ce9a3bec1b7ef78d3f18ada96aea
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2024-08-26 14:23:53 +0700

    update new

--- src/pages/admin/childAdmin/report.js
+++ src/pages/admin/childAdmin/report.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ReportPage = () => {
     return (
-        <div className="w-full h-[300px] border bg-gray-100">
+        <div className="w-full h-[300px] px-5 py-5 border bg-gray-100">
             
         </div>
     );
