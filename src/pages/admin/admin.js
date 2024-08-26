3e163bb4f5f13fd62cc36b35e2955684268bfb0c
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2024-08-26 08:43:04 +0700

    update new

--- src/pages/admin/admin.js
+++ src/pages/admin/admin.js
@@ -4,8 +4,28 @@ const AdminPage = () => {
     return (
         <div className="w-full h-full border">
             <div className="flex h-screen">
-                <div className="w-[350px] h-full bg-green-200">
-                    kk
+                <div className="w-[350px] h-full bg-green-200 px-5">
+                    <ul className="mt-10 grid grid-cols-1 gap-4">
+                        <li className="w-full h-[50px] bg-white rounded shadow hover:bg-red-100 cursor-pointer">
+
+                        </li>
+
+                        <li className="w-full h-[50px] bg-white rounded shadow hover:bg-red-100 cursor-pointer">
+
+                        </li>
+
+                        <li className="w-full h-[50px] bg-white rounded shadow hover:bg-red-100 cursor-pointer">
+
+                        </li>
+
+                        <li className="w-full h-[50px] bg-white rounded shadow hover:bg-red-100 cursor-pointer">
+
+                        </li>
+
+                        <li className="w-full h-[50px] bg-white rounded shadow hover:bg-red-100 cursor-pointer">
+
+                        </li>
+                    </ul>
                 </div>
 
                 <div className="w-full h-full">
