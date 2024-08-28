fcea08de69db56b3b1eaaa04db666f23e239f79b
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2024-08-28 09:42:36 +0700

    update new

--- src/pages/admin/admin.js
+++ src/pages/admin/admin.js
@@ -54,7 +54,7 @@ const AdminPage = () => {
                             <ul className="grid grid-cols-1 gap-2 pb-2">
                                 <li
                                     onClick={() => handleNavigation('dashboard', '/admin/dashboard')}
-                                    className={`w-full h-[50px] cursor-pointer px-5 py-2 shadow flex items-center justify-center rounded ${isTitle === "dashboard" ? 'text-yellow-500 bg-red-50' : 'hover:text-yellow-500 bg-white'}`}
+                                    className={`w-full h-[50px] cursor-pointer px-5 py-2 shadow flex items-center justify-center rounded ${isTitle === "dashboard" ? 'text-yellow-500 bg-red-50 border-t' : 'hover:text-yellow-500 bg-white'}`}
                                 >
                                     <span className="mr-auto"><PieChartIcon /></span>
                                     <span className="mr-auto font-semibold">Dashboard</span>
@@ -62,7 +62,7 @@ const AdminPage = () => {
 
                                 <li
                                     onClick={() => handleNavigation('banners', '/admin/banners')}
-                                    className={`w-full h-[50px] cursor-pointer px-5 py-2 shadow flex items-center justify-center rounded ${isTitle === "banners" ? 'text-yellow-500 bg-red-50' : 'hover:text-yellow-500 bg-white'}`}
+                                    className={`w-full h-[50px] cursor-pointer px-5 py-2 shadow flex items-center justify-center rounded ${isTitle === "banners" ? 'text-yellow-500 bg-red-50 border-t' : 'hover:text-yellow-500 bg-white'}`}
                                 >
                                     <span className="mr-auto"><CollectionsIcon /></span>
                                     <span className="mr-auto font-semibold">Banners</span>
@@ -70,7 +70,7 @@ const AdminPage = () => {
 
                                 <li
                                     onClick={() => handleNavigation('users', '/admin/users')}
-                                    className={`w-full h-[50px] cursor-pointer px-5 py-2 shadow flex items-center justify-center rounded ${isTitle === "users" ? 'text-yellow-500 bg-red-50' : 'hover:text-yellow-500 bg-white'}`}
+                                    className={`w-full h-[50px] cursor-pointer px-5 py-2 shadow flex items-center justify-center rounded ${isTitle === "users" ? 'text-yellow-500 bg-red-50 border-t' : 'hover:text-yellow-500 bg-white'}`}
                                 >
                                     <span className="mr-auto"><PeopleIcon /></span>
                                     <span className="mr-auto font-semibold">Users</span>
@@ -78,7 +78,7 @@ const AdminPage = () => {
 
                                 <li
                                     onClick={() => handleNavigation('categories', '/admin/categories')}
-                                    className={`w-full h-[50px] cursor-pointer px-5 py-2 shadow flex items-center justify-center rounded ${isTitle === "categories" ? 'text-yellow-500 bg-red-50' : 'hover:text-yellow-500 bg-white'}`}
+                                    className={`w-full h-[50px] cursor-pointer px-5 py-2 shadow flex items-center justify-center rounded ${isTitle === "categories" ? 'text-yellow-500 bg-red-50 border-t' : 'hover:text-yellow-500 bg-white'}`}
                                 >
                                     <span className="mr-auto"><CategoryIcon /></span>
                                     <span className="mr-auto font-semibold">Categories</span>
@@ -86,7 +86,7 @@ const AdminPage = () => {
 
                                 <li
                                     onClick={() => handleNavigation('originals', '/admin/originals')}
-                                    className={`w-full h-[50px] cursor-pointer px-5 py-2 shadow flex items-center justify-center rounded ${isTitle === "originals" ? 'text-yellow-500 bg-red-50' : 'hover:text-yellow-500 bg-white'}`}
+                                    className={`w-full h-[50px] cursor-pointer px-5 py-2 shadow flex items-center justify-center rounded ${isTitle === "originals" ? 'text-yellow-500 bg-red-50 border-t' : 'hover:text-yellow-500 bg-white'}`}
                                 >
                                     <span className="mr-auto"><AutoStoriesIcon /></span>
                                     <span className="mr-auto font-semibold">Originals</span>
@@ -94,7 +94,7 @@ const AdminPage = () => {
 
                                 <li
                                     onClick={() => handleNavigation('videos', '/admin/videos')}
-                                    className={`w-full h-[50px] cursor-pointer px-5 py-2 shadow flex items-center justify-center rounded ${isTitle === "videos" ? 'text-yellow-500 bg-red-50' : 'hover:text-yellow-500 bg-white'}`}
+                                    className={`w-full h-[50px] cursor-pointer px-5 py-2 shadow flex items-center justify-center rounded ${isTitle === "videos" ? 'text-yellow-500 bg-red-50 border-t' : 'hover:text-yellow-500 bg-white'}`}
                                 >
                                     <span className="mr-auto"><VideoLibraryIcon /></span>
                                     <span className="mr-auto font-semibold">Videos</span>
@@ -102,7 +102,7 @@ const AdminPage = () => {
 
                                 <li
                                     onClick={() => handleNavigation('censors', '/admin/censors')}
-                                    className={`w-full h-[50px] cursor-pointer px-5 py-2 shadow flex items-center justify-center rounded ${isTitle === "censors" ? 'text-yellow-500 bg-red-50' : 'hover:text-yellow-500 bg-white'}`}
+                                    className={`w-full h-[50px] cursor-pointer px-5 py-2 shadow flex items-center justify-center rounded ${isTitle === "censors" ? 'text-yellow-500 bg-red-50 border-t' : 'hover:text-yellow-500 bg-white'}`}
                                 >
                                     <span className="mr-auto"><SafetyCheckIcon /></span>
                                     <span className="mr-auto font-semibold">Censors</span>
@@ -110,7 +110,7 @@ const AdminPage = () => {
 
                                 <li
                                     onClick={() => handleNavigation('payments', '/admin/payments')}
-                                    className={`w-full h-[50px] cursor-pointer px-5 py-2 shadow flex items-center justify-center rounded ${isTitle === "payments" ? 'text-yellow-500 bg-red-50' : 'hover:text-yellow-500 bg-white'}`}
+                                    className={`w-full h-[50px] cursor-pointer px-5 py-2 shadow flex items-center justify-center rounded ${isTitle === "payments" ? 'text-yellow-500 bg-red-50 border-t' : 'hover:text-yellow-500 bg-white'}`}
                                 >
                                     <span className="mr-auto"><AttachMoneyIcon /></span>
                                     <span className="mr-auto font-semibold">Payments</span>
@@ -120,7 +120,7 @@ const AdminPage = () => {
 
                                 <li
                                     onClick={() => handleNavigation('notifications', '/admin/notifications')}
-                                    className={`w-full h-[50px] cursor-pointer px-5 py-2 shadow flex items-center justify-center rounded ${isTitle === "notifications" ? 'text-yellow-500 bg-red-50' : 'hover:text-yellow-500 bg-white'}`}
+                                    className={`w-full h-[50px] cursor-pointer px-5 py-2 shadow flex items-center justify-center rounded ${isTitle === "notifications" ? 'text-yellow-500 bg-red-50 border-t' : 'hover:text-yellow-500 bg-white'}`}
                                 >
                                     <span className="mr-auto"><NotificationsIcon /></span>
                                     <span className="mr-auto font-semibold">Notifications</span>
@@ -128,7 +128,7 @@ const AdminPage = () => {
 
                                 <li
                                     onClick={() => handleNavigation('report', '/admin/reports')}
-                                    className={`w-full h-[50px] cursor-pointer px-5 py-2 shadow flex items-center justify-center rounded ${isTitle === "reports" ? 'text-yellow-500 bg-red-50' : 'hover:text-yellow-500 bg-white'}`}
+                                    className={`w-full h-[50px] cursor-pointer px-5 py-2 shadow flex items-center justify-center rounded ${isTitle === "reports" ? 'text-yellow-500 bg-red-50 border-t' : 'hover:text-yellow-500 bg-white'}`}
                                 >
                                     <span className="mr-auto"><FlagIcon /></span>
                                     <span className="mr-auto font-semibold">Reports</span>
