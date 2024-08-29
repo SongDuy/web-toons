d6961c7eb0657e7f43f28f8d48a31fd9b35debfe
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2024-08-29 11:00:36 +0700

    update new

---
+++ src/pages/channel/creatorChannel.js
@@ -0,0 +1,11 @@
+import React from 'react';
+//import '../../App.css'
+const CreatorChannelPage = () => {
+    return (
+        <div className="w-full h-full border bg-gray-100">
+          
+        </div>
+    );
+}
+
+export default CreatorChannelPage;
---
+++ src/pages/channel/myChannel.js
@@ -0,0 +1,11 @@
+import React from 'react';
+//import '../../App.css'
+const MyChannelPage = () => {
+    return (
+        <div className="w-full h-full border bg-gray-100">
+           
+        </div>
+    );
+}
+
+export default MyChannelPage;
