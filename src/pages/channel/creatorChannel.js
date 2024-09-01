8b41c49bb1480abc603368c10c3f94f619b340a4
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2024-09-01 21:12:39 +0700

    update new

--- src/pages/channel/creatorChannel.js
+++ src/pages/channel/creatorChannel.js
@@ -2,8 +2,28 @@ import React from 'react';
 
 const CreatorChannelPage = () => {
     return (
-        <div className="w-full h-full border bg-gray-100">
-          
+        <div className="w-full h-full border bg-gray-100 flex items-center justify-center">
+            <div className="w-[1120px] h-full">
+                <div className="w-full h-full bg-white">
+                    <div className="w-full h-[320px] bg-green-200">
+
+                    </div>
+
+                    <div className="w-full h-[180px] bg-green-300">
+
+                    </div>
+                </div>
+
+                <div className="w-full h-[500px] flex gap-3 mt-3">
+                    <div className="w-[400px] bg-green-200">
+
+                    </div>
+
+                    <div className="w-[720px] bg-red-100">
+
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
