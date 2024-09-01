7af39fa3f0e6fea119a8f4ff54251851cc2993c1
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2024-09-01 21:17:57 +0700

    update new

--- src/pages/channel/creatorChannel.js
+++ src/pages/channel/creatorChannel.js
@@ -5,8 +5,10 @@ const CreatorChannelPage = () => {
         <div className="w-full h-full border bg-gray-100 flex items-center justify-center">
             <div className="w-[1120px] h-full">
                 <div className="w-full h-full bg-white">
-                    <div className="w-full h-[320px] bg-green-200">
-
+                    <div className="w-full h-[400px] bg-green-200">
+                        <img src="https://i.redd.it/b5jec682hfk61.jpg"
+                            className="object-cover w-[1200px] h-full rounded-t" alt="img"
+                        />
                     </div>
 
                     <div className="w-full h-[180px] bg-green-300">
