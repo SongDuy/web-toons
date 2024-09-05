1dd890bb51bf96021b12760614df72b523f10a41
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2024-09-05 11:46:46 +0700

    update new

--- src/pages/account/Delete.js
+++ src/pages/account/Delete.js
@@ -2,11 +2,22 @@ import React from "react";
 
 const Delete = () => {
 
-  return (
-    <div>
-      
-    </div>
-  );
+    return (
+        <div className="w-full h-full bg-gray-100 py-[40px] flex justify-center items-center">
+            <div className="w-[1112px] h-full">
+                {/* Tiêu đề */}
+                <span className="text-[25px] ">
+                    Account Delete
+                </span>
+
+                {/* Nội dung */}
+                <div className="w-full h-[500px] mt-[18px] px-[155px] py-[85px] bg-white">
+                    kkk
+                </div>
+            </div>
+
+        </div>
+    );
 };
 
 export default Delete;
