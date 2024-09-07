f11b451ffd6506328a787be3b7489fed47bfad49
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2024-09-07 17:40:35 +0700

    update new

--- src/components/layout/layoutUser/loading.js
+++ src/components/layout/layoutUser/loading.js
@@ -1,4 +1,4 @@
-import React,{useEffect,useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { unwrapResult } from '@reduxjs/toolkit';
 
 import { useDispatch } from 'react-redux';
@@ -6,29 +6,29 @@ import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
 import { getAllComic } from '../../../common/store/comic';
 
-const Loading = ({children}) => {
+const Loading = ({ children }) => {
     const dispatch = useDispatch();
     const [loading, setloading] = useState(false);
     useEffect(() => {
-        const get=async ()=>{
+        const get = async () => {
             try {
                 setloading(false)
-                const lg=await dispatch(getAllComic())
-              unwrapResult(lg)
+                const lg = await dispatch(getAllComic())
+                unwrapResult(lg)
                 setloading(true)
             } catch (error) {
-                
+
             }
         }
         get()
     }, [dispatch]);
     return (
         <div>
-            {loading?children:  <Box sx={{ display: 'flex',justifyContent:'center',alignItems:'center',margin:5 }}>
-      <CircularProgress />
-    </Box>
-            
-}           
+            {loading ? children : <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', margin: 5 }}>
+                <CircularProgress />
+            </Box>
+
+            }
         </div>
     );
 }
