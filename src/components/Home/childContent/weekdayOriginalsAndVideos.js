3fa4f14ec7c89cbb0c2693bf69f761be22e4a810
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2024-09-07 09:56:58 +0700

    update new

--- src/components/Home/childContent/weekdayOriginalsAndVideos.js
+++ src/components/Home/childContent/weekdayOriginalsAndVideos.js
@@ -272,17 +272,6 @@ const WeekdayOriginalsAndVideosPage = () => {
                                                 <div className="absolute inset-0 border-4 border-yellow-500 rounded-md flex items-center justify-center text-yellow-500 z-10">
                                                     <PlayArrowIcon sx={{ fontSize: 60 }} />
                                                 </div>
-
-                                                // <div class="absolute inset-0  flex items-center justify-center text-yellow-500 z-10">
-                                                //     <img
-                                                //         src={item.img}
-                                                //         alt="img"
-                                                //         class="object-fill w-full h-full rounded-md"
-                                                //     />
-                                                //     <div className="absolute w-full h-full flex items-center justify-center border-4 border-yellow-500 rounded-md">
-                                                //         <PlayArrowIcon sx={{ fontSize: 60 }} />
-                                                //     </div>
-                                                // </div>
                                             )}
                                         </div>
 
