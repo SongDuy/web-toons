3a6d0dc0083744e0422e209acfe9177183df4fab
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2024-09-13 10:26:42 +0700

    update new

--- src/pages/publish/original/episodes.js
+++ src/pages/publish/original/episodes.js
@@ -87,7 +87,7 @@ const EpisodesPage = ({ goToPreviousStep }) => {
 
                         <div className="w-9/12 h-full ">
 
-                            <div className="w-full py-3 pl-5">
+                            <div className="w-full py-3 pl-5 grid grid-cols-1 gap-5">
                                 <div className="w-full flex items-center gap-2">
                                     <h1 className="font-semibold text-xl flex items-center">
                                         Series title :
@@ -98,62 +98,32 @@ const EpisodesPage = ({ goToPreviousStep }) => {
                                     </span>
                                 </div>
 
-                                <div className="w-full mt-[30px]">
+                                <div className="w-full">
                                     <span className="w-full font-semibold text-xl">
                                         Episode title
                                     </span>
                                     <div className="flex mt-3">
-                                        <button className="w-[80px] h-[40px] border-2 bg-white flex items-center justify-center">
+                                        <button className="w-[90px] h-[40px] border-2 bg-white flex items-center justify-center">
                                             1
                                         </button>
                                         <input
-                                            className="h-[40px] px-2"
+                                            className="w-full h-[40px] px-2 border-r-2 border-t-2 border-b-2 outline-none bg-white"
                                         />
+
                                     </div>
                                 </div>
 
-                                <div className="">
+                                <div className="w-full">
 
                                 </div>
 
-                                <div className="">
+                                <div className="w-full">
 
                                 </div>
 
-                                <div className="">
+                                <div className="w-full">
 
                                 </div>
-                            </div>
-
-                            <div className="w-full mb-10 mt-5 ml-5 px-5 h-[440px] shadow bg-red-50 rounded hover:border-green-500 hover:text-gray-500 flex items-center justify-center group cursor-pointer">
-                                <div>
-                                    <span className="w-[50px] h-[50px] ml-auto mr-auto text-white bg-gray-400 rounded-full mb-3 flex items-center justify-center group-hover:bg-green-500 group-hover:text-white transition-all">
-                                        <NorthIcon />
-                                    </span>
-                                    <span className="block w-full font-semibold text-sm hover:text-gray-500">
-                                        Select an episodes original to upload.
-                                    </span>
-                                    <span className="block w-full font-semibold text-sm hover:text-gray-500">
-                                        Or drag the episodes original file here.
-                                    </span>
-                                </div>
-                            </div>
-
-                            <div className="w-full py-5 pl-5 flex border-t-2 border-gray-200">
-                                <div>
-                                    <span className="block w-full font-semibold text-sm text-green-500">
-                                        KEEP IN MIND
-                                    </span>
-                                    <span className="block py-2 font-semibold text-[15px]">
-                                        We do not allow content that contains nudity or is intended to
-                                        be sexually gratifying. This includes, but is not limited to,
-                                        full and partial nudity, as well as graphic depictions of sexual acts.
-                                        We do not allow excessive violence or graphic content intended to shock
-                                        and offend readers. This includes brutal and extended/prolonged scenes
-                                        of violence and gore. More details can be found <span className="text-blue-500">HERE</span>
-                                    </span>
-                                </div>
-
                             </div>
 
                             <div className="w-[300px] py-3 pl-5">
