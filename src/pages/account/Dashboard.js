bf016f61060d5626c7763b68621dd302cbcb0af0
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2024-09-21 20:43:56 +0700

    update new

--- src/pages/account/Dashboard.js
+++ src/pages/account/Dashboard.js
@@ -5,10 +5,10 @@ const Dashboard = () => {
     return (
         <div>
             <Nav />
-            <div className="w-full h-full border bg-gray-100 flex items-center justify-center">
+            <div className="w-full h-full border bg-gray-100 flex items-center justify-center pb-10">
                 <div className="w-[1130px] ">
                     <div className="w-full h-full mt-4">
-                        <div className="w-full h-full flex">
+                        <div className="w-full h-full flex items-center">
                             <h1 className="font-semibold">
                                 Select the original series
                             </h1>
@@ -18,9 +18,61 @@ const Dashboard = () => {
                             </button>
                         </div>
 
-                        <div className="w-full h-full">
-                            <ul className="">
-                                <li>
+                        <div className="w-full h-full mt-4">
+                            <ul className="grid grid-cols-2 gap-y-4">
+                                <li className="w-[550px] h-[210px] bg-green-200 rounded flex">
+                                    <div className="w-[210px] h-[210px] bg-red-200 rounded">
+                                        <img
+                                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIwJC19pR5MRoGAUw2-MW4DGgiUAuNZUd1dQ&s"
+                                            alt="img"
+                                            className="w-full h-full object-fill rounded"
+                                        />
+                                    </div>
+
+                                    <div className="h-full px-3 py-3">
+                                        <div className="w-[310px] h-full">
+                                            <div className="flex items-center">
+                                                <span className="text-gray-500 text-xs">
+                                                    Fantasy
+                                                </span>
+
+                                                <div className="flex border ml-auto gap-2">
+                                                    <button className="flex items-center">
+                                                        kk
+                                                    </button>
+
+                                                    <button className="flex items-center">
+                                                        ll
+                                                    </button>
+                                                </div>
+                                            </div>
+
+                                            <div className="w-full h-full">
+                                                <h1 className="font-semibold">
+                                                    kkkkk
+                                                </h1>
+                                            </div>
+                                        </div>
+
+                                        <div className="w-full h-full">
+
+                                        </div>
+
+                                        <div className="">
+
+                                        </div>
+                                    </div>
+                                </li>
+
+                                <li className="w-[550px] h-[210px] bg-green-200 rounded-xl">
+
+                                </li>
+
+                                <li className="w-[550px] h-[210px] bg-green-200 rounded-xl">
+
+                                </li>
+
+                                <li className="w-[550px] h-[210px] bg-green-200 rounded-xl">
 
                                 </li>
                             </ul>
@@ -28,7 +80,7 @@ const Dashboard = () => {
                     </div>
 
                     <div className="w-full h-full mt-4">
-                        <div className="w-full h-full flex">
+                        <div className="w-full h-full flex items-center">
                             <h1 className="font-semibold">
                                 Select the video series
                             </h1>
@@ -38,9 +90,27 @@ const Dashboard = () => {
                             </button>
                         </div>
 
-                        <div className="w-full h-full">
-                            <ul className="">
-                                <li>
+                        <div className="w-full h-full mt-4">
+                            <ul className="grid grid-cols-2 gap-y-4">
+                                <li className="w-[550px] h-[210px] bg-green-200 rounded-xl">
+                                    <div className="w-[210px] h-[210px] bg-slate-200 rounded-l-xl">
+
+                                    </div>
+
+                                    <div className="">
+
+                                    </div>
+                                </li>
+
+                                <li className="w-[550px] h-[210px] bg-green-200 rounded-xl">
+
+                                </li>
+
+                                <li className="w-[550px] h-[210px] bg-green-200 rounded-xl">
+
+                                </li>
+
+                                <li className="w-[550px] h-[210px] bg-green-200 rounded-xl">
 
                                 </li>
                             </ul>
