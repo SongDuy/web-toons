f469722ab3674193f75018fd4912c644b10438ed
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2024-09-22 15:35:41 +0700

    update new

--- src/pages/account/Episode/episodeOriginal.js
+++ src/pages/account/Episode/episodeOriginal.js
@@ -1,4 +1,4 @@
-import React,{useEffect,useState}  from 'react';
+import React, { useEffect, useState } from 'react';
 import Nav from "../../../components/Account/nav";
 import AddIcon from '@mui/icons-material/Add';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
@@ -8,7 +8,7 @@ import { getchaptersComic } from '../../../common/store/comic';
 import { unwrapResult } from '@reduxjs/toolkit';
 import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
-import { useSelector,useDispatch} from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import { useParams } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
@@ -33,138 +33,138 @@ const EpisodeOriginal = () => {
         "October",
         "November",
         "December",
-      ];
-    useEffect(() => {    
+    ];
+    useEffect(() => {
 
-        const get=async ()=>{
+        const get = async () => {
             try {
                 setloading(false)
                 const chap = await dispatch(getchaptersComic(id.id))
                 unwrapResult(chap)
-              
+
                 setloading(true)
             } catch (error) {
-                
+
             }
         }
         get()
-    }, [dispatch,id.id]);
-    const handledelete=async (idchap)=>{
+    }, [dispatch, id.id]);
+    const handledelete = async (idchap) => {
         try {
             let result = window.confirm("Do you want to delete this chap comic?");
-            if(result){
-            setloading(false)
+            if (result) {
+                setloading(false)
 
-      await comicFireBase.Deletechap(id.id,idchap)
-      const chap = await dispatch(getchaptersComic(id.id))
-      unwrapResult(chap)
-           setloading(true)
+                await comicFireBase.Deletechap(id.id, idchap)
+                const chap = await dispatch(getchaptersComic(id.id))
+                unwrapResult(chap)
+                setloading(true)
             }
         } catch (error) {
-            
+
         }
     }
     return (
         <>
-          {!loading? <Box sx={{ display: 'flex',justifyContent:'center',alignItems:'center',margin:5 }}>
-        <CircularProgress />
-        </Box>:
-        <div>
-            <Nav />
-            <div className="w-full h-full border bg-gray-100 flex items-center justify-center pb-10">
-                <div className="w-[1130px]  min-h-[550px]">
-                    <div className="w-full h-full mt-4">
-                        <div className="w-full h-full flex items-center">
-                            <h1 className="font-semibold text-xl">
-                                <Link to={`/dashboard`} className="hover:text-yellow-500">
-                                    Original Series
-                                </Link>
+            {!loading ? <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', margin: 5 }}>
+                <CircularProgress />
+            </Box> :
+                <div>
+                    <Nav />
+                    <div className="w-full h-full border bg-gray-100 flex items-center justify-center pb-10">
+                        <div className="w-[1130px]  min-h-[550px]">
+                            <div className="w-full h-full mt-4">
+                                <div className="w-full h-full flex items-center">
+                                    <h1 className="font-semibold text-xl">
+                                        <Link to={`/dashboard`} className="hover:text-yellow-500">
+                                            Original Series
+                                        </Link>
 
-                                <NavigateNextIcon sx={{ fontSize: 30 }} />
+                                        <NavigateNextIcon sx={{ fontSize: 30 }} />
 
-                                Episode Management
-                            </h1>
+                                        Episode Management
+                                    </h1>
 
-                            <div className="ml-auto flex gap-5">
-                                <button onClick={()=>navigate(`/publish/original/${id.id}`)}  className="px-2 py-1 flex items-center bg-gray-200 hover:bg-gray-300 rounded-full shadow">
-                                    <AddIcon />
-                                    Add Episode
-                                </button>
+                                    <div className="ml-auto flex gap-5">
+                                        <button onClick={() => navigate(`/publish/original/${id.id}`)} className="px-2 py-1 flex items-center bg-gray-200 hover:bg-gray-300 rounded-full shadow">
+                                            <AddIcon />
+                                            Add Episode
+                                        </button>
+                                    </div>
+
+                                </div>
+
+                                <div className="w-full h-full mt-4">
+                                    <ul className="grid grid-cols-2 gap-4">
+
+                                        {/* khung nội dung */}
+                                        {chapters?.chaps?.map((item) => (
+                                            <li className="w-full h-[210px] bg-white rounded flex shadow">
+                                                <div className="w-[210px] h-[210px] bg-red-200 rounded">
+                                                    <img
+                                                        src={item.horizontalThumbnail}
+                                                        alt="img"
+                                                        className="w-full h-full object-fill rounded"
+                                                    />
+                                                </div>
+
+                                                <div className="h-full px-3 py-3">
+                                                    <div className="w-[320px]">
+                                                        <div className="flex items-center">
+                                                            <span className="text-gray-500">
+                                                                {item.genre1}, {item.genre2}
+                                                            </span>
+
+                                                            <div className="flex ml-auto gap-2">
+                                                                <button className="px-2 flex items-center bg-gray-200 hover:bg-gray-300 rounded shadow">
+                                                                    Edit
+                                                                </button>
+
+                                                                <button onClick={() => handledelete(item.id)} className="px-2 flex items-center bg-gray-200 hover:bg-gray-300 rounded shadow">
+                                                                    Delete
+                                                                </button>
+                                                            </div>
+                                                        </div>
+
+                                                        <div className="w-full mt-2">
+                                                            <h1 className="font-semibold text-xl">
+                                                                {item.chapterTitle}
+                                                            </h1>
+                                                        </div>
+                                                    </div>
+
+
+
+                                                    <div className="w-full mt-12">
+
+                                                        <div className="flex gap-5">
+                                                            <span className="text-gray-500 text-sm flex gap-2">
+                                                                Published  {monthNames[new Date(item.createTime).getMonth()]}{" "}
+                                                                {new Date(item.createTime).getDate()},
+                                                                {new Date(item.createTime)?.getFullYear()}
+                                                            </span>
+
+                                                            <span className="text-gray-500 text-sm">
+                                                                Likes {item.likes}
+                                                            </span>
+
+                                                            <span className="text-gray-500 text-sm">
+                                                                Views {item.views}
+                                                            </span>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </div>
                             </div>
 
                         </div>
-
-                        <div className="w-full h-full mt-4">
-                            <ul className="grid grid-cols-2 gap-4">
-
-                                {/* khung nội dung */}
-                                {chapters?.chaps?.map((item) => (
-                                    <li className="w-full h-[210px] bg-white rounded flex shadow">
-                                        <div className="w-[210px] h-[210px] bg-red-200 rounded">
-                                            <img
-                                               src={item.horizontalThumbnail}
-                                                alt="img"
-                                                className="w-full h-full object-fill rounded"
-                                            />
-                                        </div>
-
-                                        <div className="h-full px-3 py-3">
-                                            <div className="w-[320px]">
-                                                <div className="flex items-center">
-                                                    <span className="text-gray-500">
-                                                        {item.genre1}, {item.genre2}
-                                                    </span>
-
-                                                    <div className="flex ml-auto gap-2">
-                                                        <button className="px-2 flex items-center bg-gray-200 hover:bg-gray-300 rounded shadow">
-                                                            Edit
-                                                        </button>
-
-                                                        <button  onClick={()=>handledelete(item.id)} className="px-2 flex items-center bg-gray-200 hover:bg-gray-300 rounded shadow">
-                                                            Delete
-                                                        </button>
-                                                    </div>
-                                                </div>
-
-                                                <div className="w-full mt-2">
-                                                    <h1 className="font-semibold text-xl">
-                                                        {item.chapterTitle}
-                                                    </h1>
-                                                </div>
-                                            </div>
-
-                                          
-
-                                            <div className="w-full mt-12">
-
-                                                <div className="flex gap-5">
-                                                    <span className="text-gray-500 text-sm flex gap-2">
-                                                        Published  {monthNames[new Date(item.createTime).getMonth()]}{" "}
-                              {new Date(item.createTime).getDate()},
-                              {new Date(item.createTime)?.getFullYear()}
-                                                    </span>
-
-                                                    <span className="text-gray-500 text-sm">
-                                                        Likes {item.likes}
-                                                    </span>
-
-                                                    <span className="text-gray-500 text-sm">
-                                                        Views {item.views}
-                                                    </span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
                     </div>
-
                 </div>
-            </div>
-        </div>
-}
-</>
+            }
+        </>
     );
 }
 
--- src/pages/auth/register.js
+++ src/pages/auth/register.js
@@ -7,6 +7,9 @@ import { useNavigate } from 'react-router-dom';
 import { unwrapResult } from '@reduxjs/toolkit';
 import useTimeout from "../../Hooks/useTimeout.js";
 
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css"; // Import CSS của react-datepicker
+
 const RegisterPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -29,22 +32,28 @@ const RegisterPage = () => {
 
     }
   }
+
+  //chọn ngày tháng năm sinh
+  const [selectedDate, setSelectedDate] = useState(null);
+
   return (
-      <div className="w-full h-full bg-gray-100 flex items-center justify-center">
-        {" "}
-        {/* backdrop-blur-sm */}
-        <div className="w-[400px] h-auto my-10 px-5 pt-5 pb-8 bg-white shadow rounded-lg">
-          <div>
-            <span className="text-[25px] flex justify-center font-semibold">
-              Sign Up
-            </span>
+    <div className="w-full h-full bg-gray-100 flex items-center justify-center">
+      {" "}
+      {/* backdrop-blur-sm */}
+      <div className="w-[400px] h-auto my-10 px-5 pt-5 pb-8 bg-white shadow rounded-lg">
+        <div>
+          <span className="text-[25px] flex justify-center font-semibold">
+            Sign Up
+          </span>
 
-            <span className="pt-3 pb-5 flex items-center justify-center">
-              Welcome! Let's take you to your registration page.
-            </span>
+          <span className="pt-3 pb-5 flex items-center justify-center">
+            Welcome! Let's take you to your registration page.
+          </span>
 
-            {/* Đăng nhập bằng email */}
-            <div className="grid grid-cols-1 gap-y-3">
+          {/* Đăng nhập bằng email */}
+          <div className="grid grid-cols-1 gap-y-3">
+
+            <div className="w-full h-[50px] grid grid-cols-2 gap-3">
               {/* Nhập tên tài khoản */}
               <input
                 type="text"
@@ -54,69 +63,83 @@ const RegisterPage = () => {
                 onChange={(e) => setdisplayName(e.target.value)}
               />
 
-              {/* Nhập địa chỉ email */}
-              <input
-                type="email"
-                className="w-full h-[50px] px-2 border rounded shadow"
-                placeholder="Email Address"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
+              {/* chọn ngày tháng năm sinh */}
+              <DatePicker
+                selected={selectedDate}
+                onChange={(date) => setSelectedDate(date)}
+                dateFormat="dd/MM/yyyy"  // Định dạng ngày
+                className="w-full h-[50px] px-2 border rounded shadow "  // Sử dụng Tailwind CSS để style
+                placeholderText="Select Date"
+                showYearDropdown  // Hiển thị danh sách năm
+                showMonthDropdown  // Hiển thị danh sách tháng
+                dropdownMode="select"  // Chuyển dropdown sang chế độ select để dễ chọn
               />
+            </div>
 
-              {/* Nhập mật khẩu */}
-              <input
-                type="password"
-                className="w-full h-[50px] px-2 border rounded shadow"
-                placeholder="Password"
-                value={password}
-                form="off"
-                onChange={(e) => setPassword(e.target.value)}
-              />
+            {/* Nhập địa chỉ email */}
+            <input
+              type="email"
+              className="w-full h-[50px] px-2 border rounded shadow"
+              placeholder="Email Address"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
 
-              <Link
-                to="/forgot-password"
-                className="w-[160px] text-blue-800 cursor-pointer"
-              >
-                Reset your password?
+            {/* Nhập mật khẩu */}
+            <input
+              type="password"
+              className="w-full h-[50px] px-2 border rounded shadow"
+              placeholder="Password"
+              value={password}
+              form="off"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <Link
+              to="/forgot-password"
+              className="w-[160px] text-blue-800 cursor-pointer"
+            >
+              Reset your password?
+            </Link>
+            {err && <p>{err.message}</p>}
+
+            <button
+              className="w-full h-[50px] bg-black text-white rounded font-semibold"
+              onClick={getRegister}
+            >
+              Continue to Verify Email
+            </button>
+
+            <div className="w-full h-[50px] flex gap-x-1 items-center justify-center border rounded">
+              <span className="">Already have an account?</span>
+
+              <Link to="/" className="text-blue-800 font-semibold">
+                Login
               </Link>
-              {err && <p>{err.message}</p>}
-
-              <button
-                className="w-full h-[50px] bg-black text-white rounded font-semibold"
-                onClick={getRegister}
-              >
-                Continue to Verify Email
-              </button>
-
-              <div className="w-full h-[50px] flex gap-x-1 items-center justify-center border rounded">
-                <span className="">Already have an account?</span>
-
-                <Link to="/" className="text-blue-800 font-semibold">
-                  Login
-                </Link>
-              </div>
             </div>
 
-            <div class="w-full my-5 flex items-center">
-              <hr class="flex-1 border-t border-gray-300" />
-              <span class="px-4">OR</span>
-              <hr class="flex-1 border-t border-gray-300" />
-            </div>
+          </div>
 
-            <div className="w-full h-full grid grid-cols-1 gap-y-5">
-              <button className="w-full h-[50px] cursor-pointer px-5 py-2 bg-red-50 hover:bg-red-100 shadow-md flex items-center justify-center rounded">
-                <span className="mr-auto">
-                  <GoogleIcon />
-                </span>
+          <div class="w-full my-5 flex items-center">
+            <hr class="flex-1 border-t border-gray-300" />
+            <span class="px-4">OR</span>
+            <hr class="flex-1 border-t border-gray-300" />
+          </div>
 
-                <span className="mr-auto font-semibold">
-                  Continue with Google
-                </span>
-              </button>
-            </div>
+          <div className="w-full h-full grid grid-cols-1 gap-y-5">
+            <button className="w-full h-[50px] cursor-pointer px-5 py-2 bg-red-50 hover:bg-red-100 shadow-md flex items-center justify-center rounded">
+              <span className="mr-auto">
+                <GoogleIcon />
+              </span>
+
+              <span className="mr-auto font-semibold">
+                Continue with Google
+              </span>
+            </button>
           </div>
         </div>
       </div>
+    </div>
   );
 };
 
