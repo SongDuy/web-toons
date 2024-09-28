b0e2baa7eaa22e9b3d3fc91b780466f1360fe2c4
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2024-09-28 07:50:45 +0700

    update new

--- src/pages/admin/childAdmin/adminUsers.js
+++ src/pages/admin/childAdmin/adminUsers.js
@@ -1,4 +1,4 @@
-import React,{useEffect,useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import LockClockIcon from '@mui/icons-material/LockClock';
 import userFireBase from '../../../common/services/User.services';
 import CircularProgress from '@mui/material/CircularProgress';
@@ -8,14 +8,14 @@ const AdminUsersPage = () => {
     const [loading, setloading] = useState(false);
     const [Users, setUsers] = useState([]);
 
-    useEffect(() => {    
+    useEffect(() => {
 
-        const get=async ()=>{
+        const get = async () => {
             try {
                 setloading(false)
-                const lg=await userFireBase.getALL()
+                const lg = await userFireBase.getALL()
                 console.log(lg)
-                setUsers(lg.success?lg?.Users:[])
+                setUsers(lg.success ? lg?.Users : [])
                 setloading(true)
             } catch (error) {
                 console.log(error)
@@ -23,64 +23,65 @@ const AdminUsersPage = () => {
         }
         get()
     }, []);
-    const handlelock=async (id,lock)=>{
+    const handlelock = async (id, lock) => {
         try {
-            let result = window.confirm(`Do you want to ${lock?"lock":"Unlocked"} this User?`);
-            if(result){
-            setloading(false)
-            await userFireBase.update({lock:!lock},id)
-            const lg=await userFireBase.getALL()
-            setUsers(lg.success?lg?.Users:[])
-           setloading(true)
-            }        } catch (error) {
-            
+            let result = window.confirm(`Do you want to ${lock ? "lock" : "Unlocked"} this User?`);
+            if (result) {
+                setloading(false)
+                await userFireBase.update({ lock: !lock }, id)
+                const lg = await userFireBase.getALL()
+                setUsers(lg.success ? lg?.Users : [])
+                setloading(true)
+            }
+        } catch (error) {
+
         }
     }
     return (
         <>
-         {loading?
-        <div className="w-full h-full py-5 bg-white">
-            <table className="w-full">
-                <thead className="bg-gray-100">
-                    <tr className="w-full">
-                        <th className="w-[50px] px-6 py-3 text-xs font-medium text-gray-500 text-center uppercase tracking-wider">ID</th>
-                        <th className="w-[300px] px-6 py-3 text-xs font-medium text-gray-500 text-center uppercase tracking-wider">Name</th>
-                        <th className="w-[300px] px-6 py-3 text-xs font-medium text-gray-500 text-center uppercase tracking-wider">Email</th>
-                        <th className="w-[300px] px-6 py-3 text-xs font-medium text-gray-500 text-center uppercase tracking-wider">Age</th>
-                        <th className="w-[300px] px-6 py-3 text-xs font-medium text-gray-500 text-center uppercase tracking-wider">Manager</th>
-                    </tr>
-                </thead>
-                <tbody className="bg-white divide-y divide-gray-200">
-                    {Users?.map((item) => (
-                        <tr key={item.uid}>
-                            <td className="px-6 py-4 whitespace-nowrap text-sm text-center font-medium text-gray-900">
-                                {item.uid}
-                            </td>
-                            <td className="px-6 py-4 whitespace-nowrap text-sm text-center text-gray-500">
-                                {item.name}
-                            </td>
-                            <td className="px-6 py-4 whitespace-nowrap text-sm text-center text-gray-500">
-                                {item.email}
-                            </td>
-                            <td className="px-6 py-4 whitespace-nowrap text-sm text-center text-gray-500">
-                                {item?.birthday? new Date(Date.now())?.getFullYear()-new Date(item.birthday)?.getFullYear():'Not Birthday '}
-                            </td>
-                            <td className="px-6 py-4 whitespace-nowrap text-sm text-center text-gray-500">
-                              
-                                <button onClick={()=>handlelock(item.uid,item.lock)} className={`w-[35px] h-[35px] ${item?.lock? "text-blue-500":"text-red-500"} mx-1 bg-gray-100 hover:bg-gray-200 rounded-full`}>
-                                    <LockClockIcon />
-                                </button>
-                                
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
-           : <Box sx={{ display: 'flex',justifyContent:'center',alignItems:'center',margin:5 }}>
-           <CircularProgress />
-           </Box>}
-                   </>
+            {loading ?
+                <div className="w-full h-full py-5 bg-white">
+                    <table className="w-full">
+                        <thead className="bg-gray-100">
+                            <tr className="w-full">
+                                <th className="w-[50px] px-6 py-3 text-xs font-medium text-gray-500 text-center uppercase tracking-wider">ID</th>
+                                <th className="w-[300px] px-6 py-3 text-xs font-medium text-gray-500 text-center uppercase tracking-wider">Name</th>
+                                <th className="w-[300px] px-6 py-3 text-xs font-medium text-gray-500 text-center uppercase tracking-wider">Email</th>
+                                <th className="w-[300px] px-6 py-3 text-xs font-medium text-gray-500 text-center uppercase tracking-wider">Age</th>
+                                <th className="w-[300px] px-6 py-3 text-xs font-medium text-gray-500 text-center uppercase tracking-wider">Manager</th>
+                            </tr>
+                        </thead>
+                        <tbody className="bg-white divide-y divide-gray-200">
+                            {Users?.map((item) => (
+                                <tr key={item.uid}>
+                                    <td className="px-6 py-4 whitespace-nowrap text-sm text-center font-medium text-gray-900">
+                                        {item.uid}
+                                    </td>
+                                    <td className="px-6 py-4 whitespace-nowrap text-sm text-center text-gray-500">
+                                        {item.name}
+                                    </td>
+                                    <td className="px-6 py-4 whitespace-nowrap text-sm text-center text-gray-500">
+                                        {item.email}
+                                    </td>
+                                    <td className="px-6 py-4 whitespace-nowrap text-sm text-center text-gray-500">
+                                        {item?.birthday ? new Date(Date.now())?.getFullYear() - new Date(item.birthday)?.getFullYear() : 'Not Birthday '}
+                                    </td>
+                                    <td className="px-6 py-4 whitespace-nowrap text-sm text-center text-gray-500">
+
+                                        <button onClick={() => handlelock(item.uid, item.lock)} className={`w-[35px] h-[35px] ${item?.lock ? "text-blue-500" : "text-red-500"} mx-1 bg-gray-100 hover:bg-gray-200 rounded-full`}>
+                                            <LockClockIcon />
+                                        </button>
+
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+                : <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', margin: 5 }}>
+                    <CircularProgress />
+                </Box>}
+        </>
     );
 }
 
--- src/pages/admin/childAdmin/adminVideosidchap.js
+++ src/pages/admin/childAdmin/adminVideosidchap.js
@@ -1,10 +1,10 @@
-import React,{useEffect,useState} from 'react';
+import React, { useEffect, useState } from 'react';
 
-import { useDispatch} from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { unwrapResult } from '@reduxjs/toolkit';
 import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
-import  {  getchaptersVideo } from '../../../common/store/Video';
+import { getchaptersVideo } from '../../../common/store/Video';
 
 import { useParams } from 'react-router-dom';
 import ReactPlayer from "react-player";
@@ -15,47 +15,47 @@ const AdminVideosPageidchap = () => {
     const id = useParams();
     const [chapid, setchapid] = useState([]);
 
-    useEffect(() => {    
+    useEffect(() => {
 
-        const get=async ()=>{
+        const get = async () => {
             try {
                 setloading(false)
                 const chap = await dispatch(getchaptersVideo(id.id));
 
                 const chapid = unwrapResult(chap);
                 setchapid(
-                  chapid.success
-                    ? chapid?.chaps.filter((item) => item.id === id.idseries)[0]
-                    : []
+                    chapid.success
+                        ? chapid?.chaps.filter((item) => item.id === id.idseries)[0]
+                        : []
                 );
                 setloading(true)
             } catch (error) {
-                
+
             }
         }
         get()
-    }, [dispatch,id]);
-   
-    
+    }, [dispatch, id]);
+
+
     return (
         <>
-           {loading?
-        <div className="w-full h-[600px] py-5 bg-white custom-scrollbar">
-           <ReactPlayer
-              url={
-                chapid?.fileURL
-                  ? chapid?.fileURL
-                  : "https://www.youtube.com/watch?v=CnDNIEe7G0I"
-              }
-              controls={true}
-              width="80%"
-              height="100%"
-            />
-        </div>
-        : <Box sx={{ display: 'flex',justifyContent:'center',alignItems:'center',margin:5 }}>
-        <CircularProgress />
-        </Box>}
-                </>
+            {loading ?
+                <div className="w-full h-[600px] py-5 bg-white custom-scrollbar">
+                    <ReactPlayer
+                        url={
+                            chapid?.fileURL
+                                ? chapid?.fileURL
+                                : "https://www.youtube.com/watch?v=CnDNIEe7G0I"
+                        }
+                        controls={true}
+                        width="80%"
+                        height="100%"
+                    />
+                </div>
+                : <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', margin: 5 }}>
+                    <CircularProgress />
+                </Box>}
+        </>
     );
 }
 
