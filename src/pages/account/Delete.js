02bb8c954badaf23aa0c24f69a1c582ba4cb649d
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2024-10-05 12:25:53 +0700

    update new

--- src/pages/account/Delete.js
+++ src/pages/account/Delete.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useSelector } from "react-redux";
 
 import { Link } from "react-router-dom";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
@@ -34,7 +35,7 @@ const Delete = () => {
   const [isChecked, setIsChecked] = useState(false);
   const [Password, setPassword] = useState("");
   const [error, seterror] = useState("");
-const [checkgoogle, setcheckgoogle] = useState(false);
+  const [checkgoogle, setcheckgoogle] = useState(false);
   const dispatch = useDispatch();
   const [open, setOpen] = useState(false);
 
@@ -59,8 +60,8 @@ const [checkgoogle, setcheckgoogle] = useState(false);
 
     if (user) {
       try {
-         const credential =!checkgoogle&& EmailAuthProvider.credential(user.email, Password); // Giả sử bạn đã lấy được mật khẩu từ người dùng
-        !checkgoogle&&  await reauthenticateWithCredential(user, credential);
+        const credential = !checkgoogle && EmailAuthProvider.credential(user.email, Password); // Giả sử bạn đã lấy được mật khẩu từ người dùng
+        !checkgoogle && await reauthenticateWithCredential(user, credential);
         await CommentFireBase.deleteAccount(auth.currentUser.uid);
         await comicFireBase.deleteAccount(auth.currentUser.uid);
         await postFireBase.deleteAccount(auth.currentUser.uid);
@@ -70,9 +71,9 @@ const [checkgoogle, setcheckgoogle] = useState(false);
         await VideoFireBase.deleteAccount(auth.currentUser.uid);
         await FollowFireBase.deleteAccount(auth.currentUser.uid);
         await PaymentFireBase.deleteAccount(auth.currentUser.uid)
-        !checkgoogle&&await deleteUser(user);
+        !checkgoogle && await deleteUser(user);
         dispatch(logout());
-          setOpen(false);
+        setOpen(false);
 
         // Thực hiện các hành động cần thiết sau khi xóa tài khoản, ví dụ: chuyển hướng người dùng, xóa dữ liệu liên quan, ...
       } catch (error) {
@@ -85,45 +86,85 @@ const [checkgoogle, setcheckgoogle] = useState(false);
       // Xử lý trường hợp không có người dùng đăng nhập
     }
   };
+
+  //Lấy ngôn ngữ
+  const language = useSelector((state) => state.hidden.language);
+
   return (
     <div className="w-full h-full bg-gray-100 py-[40px] flex justify-center items-center">
       <div className="w-[1112px] h-full">
         {/* Tiêu đề */}
-        <h1 className="text-[22px] font-semibold">Account Delete</h1>
+        <h1 className="text-[22px] font-semibold">
+
+          {!language ?
+            (<span> Account Delete </span>)
+            :
+            (<span> 계정 삭제 </span>)
+          }
+        </h1>
 
         {/* Nội dung */}
         <div className="w-full h-full mt-[15px] px-[155px] py-[80px] bg-white">
           <div className="w-full grid grid-cols-1 gap-2">
             <h1 className="text-[18px] font-semibold">
-              Are you sure you want to delete your account?
+              {!language ?
+                (<span> Are you sure you want to delete your account? </span>)
+                :
+                (<span> 정말로 계정을 삭제하시겠습니까? </span>)
+              }
+
             </h1>
             <span className="w-full">
-              Deleting your account is permanent and will remove all
-              subscription history.
+              {!language ?
+                (<span> Deleting your account is permanent and will remove all subscription history. </span>)
+                :
+                (<span> 계정을 삭제하면 구독 기록이 영구적으로 삭제됩니다.</span>)
+              }
+
             </span>
           </div>
 
           <div className="w-full mt-[60px] grid grid-cols-1 gap-2">
-            <h1 className="text-[18px] font-semibold">Before you go...</h1>
+            <h1 className="text-[18px] font-semibold">
+              {!language ?
+                (<span> Before you go... </span>)
+                :
+                (<span> 가기 전에... </span>)
+              }
+
+            </h1>
             <ul className="grid grid-cols-1 gap-1">
               <li className="flex flex-wrap">
-                <span>
-                  &#9679; If you wish to unsubscribe from receiving email
-                  notifications, you can change your
-                </span>
-                <span>
-                  &nbsp;&nbsp; settings at{" "}
-                  <Link to={`/account`} className="text-blue-500">
-                    {" "}
-                    ACCOUNT <NavigateNextIcon /> Email Notification.{" "}
-                  </Link>
-                </span>
+                {!language ?
+                  (<span className="min-w-full"> &#9679; If you wish to unsubscribe from receiving email notifications, you can change your </span>)
+                  :
+                  (<span className="min-w-full"> &#9679; 이메일 알림 수신 구독을 취소하려면 다음을 변경할 수 있습니다 </span>)
+                }
+
+                {!language ?
+                  (<span>
+                    &nbsp;&nbsp; settings at{" "}
+                    <Link to={`/account`} className="text-blue-500">
+                      {" "}
+                      ACCOUNT <NavigateNextIcon /> Email Notification.{" "}
+                    </Link>
+                  </span>)
+                  :
+                  (<span>
+                    &nbsp;&nbsp; 설정 위치{" "}
+                    <Link to={`/account`} className="text-blue-500">
+                      {" "}
+                      계정 <NavigateNextIcon /> 이메일 알림.{" "}
+                    </Link>
+                  </span>)
+                }
+
               </li>
               <li className="flex flex-wrap">
                 <span>
-                  &#9679; Any comments you wrote will not be deleted
-                  automatically.
+                  &#9679; Any comments you wrote will not be deleted automatically.
                 </span>
+                
                 <span>
                   &nbsp;&nbsp; Please go to{" "}
                   <Link to={`/mycomment`} className="text-blue-500">
@@ -154,9 +195,8 @@ const [checkgoogle, setcheckgoogle] = useState(false);
           <div>
             <div className="mt-[60px]">
               <button
-                className={`w-[35px] h-[35px] border-2 rounded-full ${
-                  isChecked ? "bg-green-500 text-white" : ""
-                }`}
+                className={`w-[35px] h-[35px] border-2 rounded-full ${isChecked ? "bg-green-500 text-white" : ""
+                  }`}
                 onClick={handleCheckboxClick}
               >
                 <CheckIcon />
@@ -174,9 +214,8 @@ const [checkgoogle, setcheckgoogle] = useState(false);
               </Link>
 
               <button
-                className={`w-[240px] h-[50px] ${
-                  isChecked ? "bg-black" : "bg-gray-200 cursor-not-allowed"
-                } rounded-full shadow text-white font-semibold`}
+                className={`w-[240px] h-[50px] ${isChecked ? "bg-black" : "bg-gray-200 cursor-not-allowed"
+                  } rounded-full shadow text-white font-semibold`}
                 disabled={!isChecked}
                 onClick={handleClickOpen}
               >
@@ -195,27 +234,27 @@ const [checkgoogle, setcheckgoogle] = useState(false);
         <DialogTitle id="alert-dialog-title">
           {"Re-enter password?"}
         </DialogTitle>
-        {!checkgoogle&&
-        <DialogContent>
-    
-          <TextField
-            autoFocus
-            margin="dense"
-            id="password"
-            label="Re-enter password"
-            type="password"
-            fullWidth
-            value={Password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          {error && (
-            <DialogContentText id="alert-dialog-description">
-              {error}
-            </DialogContentText>
-          )}
-          
-        </DialogContent>
-}
+        {!checkgoogle &&
+          <DialogContent>
+
+            <TextField
+              autoFocus
+              margin="dense"
+              id="password"
+              label="Re-enter password"
+              type="password"
+              fullWidth
+              value={Password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            {error && (
+              <DialogContentText id="alert-dialog-description">
+                {error}
+              </DialogContentText>
+            )}
+
+          </DialogContent>
+        }
         <DialogActions>
           <Button onClick={handleClose}>Disagree</Button>
           <Button onClick={deleteAccount} autoFocus>
