d92b566f2d5f289c3a53bf04c7af413b00098a31
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2024-10-09 12:23:39 +0700

    update new

--- src/pages/channel/myChannel.js
+++ src/pages/channel/myChannel.js
@@ -347,14 +347,14 @@ const MyChannelPage = () => {
                           <div className="h-full rounded-xl px-3 py-3 flex items-center">
                             <div className="w-[250px] overflow-hidden ">
                               <span className="w-full text-lg font-semibold line-clamp-1">
-                                {item.title} kk  kk  k k k kk  kk  kk k  k kk k 
+                                {item.title}
                               </span>
                               {(item.genre1 === item.genre2) ?
-                                <span className="w-full border text-[15px] line-clamp-1">
+                                <span className="w-full text-[15px] line-clamp-1">
                                   {!language ? item.genre1 : dataListGenre?.filter(itm => itm.name.toLowerCase() === item.genre1.toLowerCase())[0]?.nameKorean}
                                 </span>
                                 :
-                                <span className="w-full border text-[15px] line-clamp-1">
+                                <span className="w-full text-[15px] line-clamp-1">
                                   {!language ? item.genre1 : dataListGenre?.filter(itm => itm.name.toLowerCase() === item.genre1.toLowerCase())[0]?.nameKorean}
                                   {`, `}
                                   {!language ? item.genre2 : dataListGenre?.filter(itm => itm.name.toLowerCase() === item.genre2.toLowerCase())[0]?.nameKorean}
