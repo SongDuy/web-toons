ae73730238f89a349120da38f7a78d7aa25cf8e1
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2024-10-12 09:51:03 +0700

    update new

--- src/pages/publish/original/episodes.js
+++ src/pages/publish/original/episodes.js
@@ -315,51 +315,53 @@ const EpisodesOriginalPage = ({ goToPreviousStep }) => {
       ) : (
         <div className="w-full h-full bg-gray-100 pb-10">
           <div className="w-full h-[70px] bg-white shadow flex items-center justify-center border-t">
-            <ul className="flex gap-10">
+            <ul className="w-full flex items-center justify-center xs:gap-0 sm:gap-10">
               <li
                 onClick={goToPreviousStep}
-                className="uppercase font-semibold cursor-pointer text-md flex items-center justify-center"
+                className="min-w-[120px] uppercase font-semibold cursor-pointer flex items-center justify-center"
               >
                 <div className="w-[40px] h-[40px] bg-gray-500 rounded-full border flex items-center justify-center mx-2">
                   <span className="mx-3 text-2xl text-white font-bold">1</span>
                 </div>
 
-                <h1 className="text-gray-400">
+                <h1 className="text-gray-400 xs:text-sm sm:text-md">
                   {!language ? (
-                    "ORIGINAL SERIES"
+                    <span>ORIGINAL SERIES</span>
                   ) : (
-                    "오리지널 시리즈"
+                    <span>오리지널 시리즈</span>
                   )}
                 </h1>
 
               </li>
 
-              <li className="uppercase font-semibold text-md flex items-center justify-center">
+              <li className="xs:ml-2 sm:ml-0 uppercase font-semibold flex items-center justify-center">
                 <span className="text-gray-400">
                   <ArrowForwardIosIcon />
                 </span>
               </li>
-              <li className="uppercase font-semibold cursor-pointer text-md flex items-center justify-center">
+              <li className="min-w-[120px] uppercase font-semibold cursor-pointer text-md flex items-center justify-center">
                 <div className="w-[40px] h-[40px] bg-green-500 rounded-full border flex items-center justify-center mx-2">
                   <span className="mx-3 text-2xl text-white font-bold">2</span>
                 </div>
 
-                <h1 className="text-black">
+                <h1 className="text-black xs:text-sm sm:text-md">
                   {!language ? (
-                    "ORIGINAL EPISODES"
+                    <span>ORIGINAL EPISODES</span>
                   ) : (
-                    "오리지널 에피소드"
+                    <span>오리지널 에피소드</span>
                   )}
                 </h1>
               </li>
             </ul>
           </div>
 
-          <div className="w-full h-full px-[200px]">
-            <div className="w-full h-full py-5 flex">
-              <div className="w-3/12 h-full">
+          <div className="w-full h-full sm:px-[200px] xs:px-[10px] sm:ml-0">
+            <div className="w-full h-full py-5 grid grid-cols-1 lg:grid-cols-3">
+
+              {/* Cột bên trái */}
+              <div className="w-full h-full col-span-1 ">
                 {/* Phần tải ảnh cho tập truyện */}
-                <div className="w-[220px] h-full">
+                <div className="w-full h-full">
                   <div className="w-full py-3">
                     <span className="w-full font-semibold text-xl">
                       {!language ? (
@@ -369,50 +371,52 @@ const EpisodesOriginalPage = ({ goToPreviousStep }) => {
                       )}
                     </span>
                   </div>
-                  {photos1 ? (
-                    <img
-                      src={photos1}
-                      alt="Selected"
-                      className="w-[200px] h-[200px] object-cover rounded"
-                    />
-                  ) : (
-                    <>
-                      <button className="w-[200px] h-[200px] relative shadow border bg-red-50 rounded hover:border-green-500 hover:text-gray-500 flex items-center justify-center group">
-                        <div>
-                          <span className="w-[50px] h-[50px] ml-auto mr-auto text-white bg-gray-400 rounded-full mb-3 flex items-center justify-center group-hover:bg-green-500 group-hover:text-white transition-all">
-                            <NorthIcon />
-                          </span>
 
-                          <span className="block w-full font-semibold text-sm hover:text-gray-500">
-                            {!language ? (
-                              "Select an image to upload."
-                            ) : (
-                              "업로드할 이미지를 선택하세요."
-                            )}
-                          </span>
+                  <div className="w-full flex items-center justify-center">
+                    {photos1 ? (
+                      <img
+                        src={photos1}
+                        alt="Selected"
+                        className="w-[200px] h-[200px] object-cover rounded"
+                      />
+                    ) : (
+                      <>
+                        <button className="w-[200px] h-[200px] relative shadow border bg-red-50 rounded hover:border-green-500 hover:text-gray-500 flex items-center justify-center group">
+                          <div>
+                            <span className="w-[50px] h-[50px] ml-auto mr-auto text-white bg-gray-400 rounded-full mb-3 flex items-center justify-center group-hover:bg-green-500 group-hover:text-white transition-all">
+                              <NorthIcon />
+                            </span>
 
-                          <span className="block w-full font-semibold text-sm hover:text-gray-500">
-                            {!language ? (
-                              "Or drag the image file here."
-                            ) : (
-                              "또는 이미지를 여기로 드래그하세요."
-                            )}
-                          </span>
+                            <span className="block w-full font-semibold text-sm hover:text-gray-500">
+                              {!language ? (
+                                "Select an image to upload."
+                              ) : (
+                                "업로드할 이미지를 선택하세요."
+                              )}
+                            </span>
 
-                          <input
-                            type="file"
-                            accept="image/*"
-                            onChange={(e) => handlePhotoChange1(e)}
-                            className="absolute inset-0 opacity-0 cursor-pointer "
-                          />
-                        </div>
-                      </button>
-                    </>
-                  )}
+                            <span className="block w-full font-semibold text-sm hover:text-gray-500">
+                              {!language ? (
+                                "Or drag the image file here."
+                              ) : (
+                                "또는 이미지를 여기로 드래그하세요."
+                              )}
+                            </span>
 
+                            <input
+                              type="file"
+                              accept="image/*"
+                              onChange={(e) => handlePhotoChange1(e)}
+                              className="absolute inset-0 opacity-0 cursor-pointer "
+                            />
+                          </div>
+                        </button>
+                      </>
+                    )}
+                  </div>
                   <div className="w-full py-3">
 
-                    <span className="block w-full font-semibold text-[13.5px] text-gray-600">
+                    <span className="block w-full font-semibold text-gray-600">
                       {!language ? (
                         "Recommended size is 160x151."
                       ) : (
@@ -420,7 +424,7 @@ const EpisodesOriginalPage = ({ goToPreviousStep }) => {
                       )}
                     </span>
 
-                    <span className="block w-full font-semibold text-[13.5px] text-gray-600">
+                    <span className="block w-full font-semibold text-gray-600">
                       {!language ? (
                         "Image must be less than 500KB."
                       ) : (
@@ -428,19 +432,28 @@ const EpisodesOriginalPage = ({ goToPreviousStep }) => {
                       )}
                     </span>
 
-                    <span className="block w-full font-semibold text-[13.5px] text-gray-600">
+                    <span className="block w-full font-semibold text-gray-600">
                       {!language ? (
-                        "Only JPG, JPEG, and PNG formats are allowed. File name can only be in English letters and numbers."
+                        <span>Only JPG, JPEG, and PNG formats are allowed.</span>
                       ) : (
-                        "JPG, JPEG, PNG 형식만 허용됩니다. 파일 이름은 영어 문자와 숫자만 사용할 수 있습니다."
+                        <span>JPG, JPEG, PNG 형식만 허용됩니다.</span>
+                      )}
+                    </span>
+                    <span className="block w-full font-semibold text-gray-600">
+                      {!language ? (
+                        <span className="flex-wrap">
+                          <span>File name can only be in English letters and numbers.</span>
+                        </span>
+                      ) : (
+                        <span>파일 이름은 영어 문자와 숫자만 사용할 수 있습니다.</span>
                       )}
                     </span>
                   </div>
                 </div>
               </div>
 
-              {/* Phần ghi chú của tác giả */}
-              <div className="w-full py-3 pl-5 border-b-2 pb-10">
+              {/* Cột bên phải */}
+              <div className="w-full h-full col-span-2 py-3 pl-5 border-b-2 pb-10">
                 <div className="grid grid-cols-1 gap-5">
                   <div className="w-full flex items-center gap-2">
 
@@ -470,7 +483,7 @@ const EpisodesOriginalPage = ({ goToPreviousStep }) => {
 
                     <div className="flex mt-3">
                       <button className="w-[90px] h-[40px] border-2 bg-white flex items-center justify-center">
-                        {id.idchap ? num :chapters?.chaps?.length? chapters?.chaps?.length + 1:1}
+                        {id.idchap ? num : chapters?.chaps?.length ? chapters?.chaps?.length + 1 : 1}
                       </button>
                       <input
                         className="w-full h-[40px] px-2 border-r-2 border-t-2 border-b-2 outline-none bg-white"
