7be7d2098992fd56b0329e3517402acbb1f7391a
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2024-10-13 15:35:42 +0700

    update new

--- src/components/layout/layoutUser/19Age.js
+++ src/components/layout/layoutUser/19Age.js
@@ -8,98 +8,98 @@ import { setcheck19Modal } from '../../../common/store/hidden';
 import { getAccount } from '../../../common/store/Account';
 import { unwrapResult } from '@reduxjs/toolkit';
 
-const Login19AgePage = ({ closeModal,check }) => {
+const Login19AgePage = ({ closeModal, check }) => {
     // Mở và đóng modal login 19 tuổi
     const dispatch = useDispatch();
 
-    const handleBackdropClick =async (event) => {
+    const handleBackdropClick = async (event) => {
         try {
             if (event.target === event.currentTarget && auth?.currentUser) {
-                if(check){
-                    await userFireBase.update({checkage:true},auth?.currentUser?.uid)
+                if (check) {
+                    await userFireBase.update({ checkage: true }, auth?.currentUser?.uid)
                     dispatch(setcheck19Modal(true))
                     const account = await dispatch(getAccount(auth?.currentUser?.uid));
                     unwrapResult(account);
                     closeModal();
-                }else{
-                    await userFireBase.update({checkage:false},auth?.currentUser?.uid)
+                } else {
+                    await userFireBase.update({ checkage: false }, auth?.currentUser?.uid)
                     dispatch(setcheck19Modal(false))
                     const account = await dispatch(getAccount(auth?.currentUser?.uid));
-                     unwrapResult(account); 
+                    unwrapResult(account);
                     closeModal();
                 }
-              
+
             }
         } catch (error) {
-            
+
         }
     };
 
     const language = useSelector(state => state.hidden.language);
     return (
-      <>
-      {check?  <div
-            className="w-screen h-screen bg-black bg-opacity-30 flex items-center justify-center fixed inset-0 z-50"
-            onClick={handleBackdropClick}
-        >
-            <div className="w-[350px] bg-white rounded-lg shadow-lg pt-10 pb-10 text-center transform transition-all duration-300">
-                <h2 className="text-2xl font-bold mb-2">
-                    {!language ? "Login Successful!" : "로그인 성공!"}
-                </h2>
-                <p className="text-gray-600">
-                    {!language
-                        ? "You have successfully logged in to access 19+ content."
-                        : "19+ 콘텐츠에 대한 로그인에 성공했습니다."}
-                </p>
+        <>
+            {check ? <div
+                className="w-screen h-screen bg-black bg-opacity-30 flex items-center justify-center fixed inset-0 z-50"
+                onClick={handleBackdropClick}
+            >
+                <div className="w-[350px] bg-white rounded-lg shadow-lg pt-10 pb-10 text-center transform transition-all duration-300">
+                    <h2 className="text-2xl font-bold mb-2">
+                        {!language ? "Login Successful!" : "로그인 성공!"}
+                    </h2>
+                    <p className="text-gray-600">
+                        {!language
+                            ? "You have successfully logged in to access 19+ content."
+                            : "19+ 콘텐츠에 대한 로그인에 성공했습니다."}
+                    </p>
 
-                <div className="w-full mt-2 flex items-center justify-center gap-2">
-                    <div className="w-[35px] h-[35px] text-white rounded-full bg-gradient-to-t from-yellow-200 via-yellow-400 to-yellow-500 flex items-center justify-center">
-                        <CheckIcon />
+                    <div className="w-full mt-2 flex items-center justify-center gap-2">
+                        <div className="w-[35px] h-[35px] text-white rounded-full bg-gradient-to-t from-yellow-200 via-yellow-400 to-yellow-500 flex items-center justify-center">
+                            <CheckIcon />
+                        </div>
+                        <h1 className="text-shadow-black text-yellow-500 text-xl">19+</h1>
                     </div>
-                    <h1 className="text-shadow-black text-yellow-500 text-xl">19+</h1>
-                </div>
 
-                <div className="w-full mt-10 flex items-center justify-center">
-                    <button
-                        className="w-1/2 h-[50px] text-white font-semibold bg-red-500 hover:bg-red-600 shadow rounded-full flex items-center justify-center"
-                        onClick={handleBackdropClick}
-                    >
-                        {!language ? "Cancel" : "취소"}
-                    </button>
-                </div>
-            </div>
-        </div>:  <div
-            className="w-screen h-screen bg-black bg-opacity-30 flex items-center justify-center fixed inset-0 z-50"
-            onClick={handleBackdropClick}
-        >
-            <div className="w-[350px] bg-white rounded-lg shadow-lg pt-10 pb-10 text-center transform transition-all duration-300">
-                <h2 className="text-2xl font-bold mb-2">
-                    {!language ? "Login Successful!" : "로그인 성공!"}
-                </h2>
-                <p className="text-gray-600">
-                    {!language
-                        ? "You have successfully logged in to access 19+ content."
-                        : "19+ 콘텐츠에 대한 로그인에 성공했습니다."}
-                </p>
-
-                <div className="w-full mt-2 flex items-center justify-center gap-2">
-                    <div className="w-[35px] h-[35px] text-white rounded-full bg-gradient-to-t from-yellow-200 via-yellow-400 to-yellow-500 flex items-center justify-center">
-                        <CheckIcon />
+                    <div className="w-full mt-10 flex items-center justify-center">
+                        <button
+                            className="w-1/2 h-[50px] text-white font-semibold bg-red-500 hover:bg-red-600 shadow rounded-full flex items-center justify-center"
+                            onClick={handleBackdropClick}
+                        >
+                            {!language ? "Agree" : "동의하다"}
+                        </button>
                     </div>
-                    <h1 className="text-shadow-black text-yellow-500 text-xl">19+</h1>
                 </div>
+            </div> : <div
+                className="w-screen h-screen bg-black bg-opacity-30 flex items-center justify-center fixed inset-0 z-50"
+                onClick={handleBackdropClick}
+            >
+                <div className="w-[350px] bg-white rounded-lg shadow-lg pt-10 pb-10 text-center transform transition-all duration-300">
+                    <h2 className="text-2xl font-bold mb-2">
+                        {!language ? "Login Successful!" : "로그인 성공!"}
+                    </h2>
+                    <p className="text-gray-600">
+                        {!language
+                            ? "You have successfully logged in to access 19+ content."
+                            : "19+ 콘텐츠에 대한 로그인에 성공했습니다."}
+                    </p>
 
-                <div className="w-full mt-10 flex items-center justify-center">
-                    <button
-                        className="w-1/2 h-[50px] text-white font-semibold bg-red-500 hover:bg-red-600 shadow rounded-full flex items-center justify-center"
-                        onClick={handleBackdropClick}
-                    >
-                        {!language ? "Cancel" : "취소"}
-                    </button>
+                    <div className="w-full mt-2 flex items-center justify-center gap-2">
+                        <div className="w-[35px] h-[35px] text-white rounded-full bg-gradient-to-t from-yellow-200 via-yellow-400 to-yellow-500 flex items-center justify-center">
+                            <CheckIcon />
+                        </div>
+                        <h1 className="text-shadow-black text-yellow-500 text-xl">19+</h1>
+                    </div>
+
+                    <div className="w-full mt-10 flex items-center justify-center">
+                        <button
+                            className="w-1/2 h-[50px] text-white font-semibold bg-red-500 hover:bg-red-600 shadow rounded-full flex items-center justify-center"
+                            onClick={handleBackdropClick}
+                        >
+                            {!language ? "Agree" : "동의하다"}
+                        </button>
+                    </div>
                 </div>
-            </div>
-        </div>}
-      </>
+            </div>}
+        </>
 
     );
 }
