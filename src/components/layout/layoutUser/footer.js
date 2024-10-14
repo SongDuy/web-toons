2825e80e44ce31806806cf0f7090a52a354f4a0f
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2024-10-14 20:40:00 +0700

    update new

--- src/components/layout/layoutUser/footer.js
+++ src/components/layout/layoutUser/footer.js
@@ -1,11 +1,16 @@
 import React from 'react';
-//import '../../App.css'
+import { useSelector } from 'react-redux';
+
 import { Link } from 'react-router-dom';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import YouTubeIcon from '@mui/icons-material/YouTube';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import TwitterIcon from '@mui/icons-material/Twitter';
 const FooterPage = () => {
+
+    //Lấy ngôn ngữ
+    const language = useSelector(state => state.hidden.language);
+
     return (
         <footer className="w-full border-t-2 bg-gray-100 text-black py-10">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -46,35 +51,59 @@ const FooterPage = () => {
                     <ul className="grid xs:grid-cols-4 sm:grid-cols-5 gap-x-5 gap-y-3 mt-auto">
                         <Link to={`#`}>
                             <li className="h-full hover:text-yellow-500 flex items-center justify-center">
-                                About
-                            </li>
-                        </Link>
+                                {!language ? (
+                                    <span>About</span>
 
+                                ) : (
+                                    <span>정보</span>
+                                )}
 
-                        <Link to={`#`}>
-                            <li className="h-full hover:text-yellow-500 flex items-center justify-center">
-                                Terms
-                            </li>
-                        </Link>
-
-                        <Link to={`#`}>
-                            <li className="h-full hover:text-yellow-500 flex items-center justify-center">
-                                Privacy
                             </li>
                         </Link>
 
                         <Link to={`#`}>
                             <li className="h-full hover:text-yellow-500 flex items-center justify-center">
-                                Advertise
+                                {!language ? (
+                                    <span>Terms</span>
+
+                                ) : (
+                                    <span>약관</span>
+                                )}
                             </li>
                         </Link>
 
                         <Link to={`#`}>
                             <li className="h-full hover:text-yellow-500 flex items-center justify-center">
-                                Contact
+                                {!language ? (
+                                    <span>Privacy</span>
+
+                                ) : (
+                                    <span>개인정보 보호</span>
+                                )}
                             </li>
                         </Link>
 
+                        <Link to={`#`}>
+                            <li className="h-full hover:text-yellow-500 flex items-center justify-center">
+                                {!language ? (
+                                    <span>Advertise</span>
+
+                                ) : (
+                                    <span>광고하다</span>
+                                )}
+                            </li>
+                        </Link>
+
+                        <Link to={`#`}>
+                            <li className="h-full hover:text-yellow-500 flex items-center justify-center">
+                                {!language ? (
+                                    <span>Contact</span>
+
+                                ) : (
+                                    <span>연락처</span>
+                                )}
+                            </li>
+                        </Link>
                     </ul>
                 </div>
 
@@ -83,7 +112,7 @@ const FooterPage = () => {
                     ⓒ 2024 Rison. 모든 권리는 보호됩니다.
                 </div>
             </div>
-        </footer>
+        </footer >
     );
 }
 
