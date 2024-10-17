f6a7db80059f064ad072d4f74fcf7ef6770aba57
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2024-10-17 09:11:45 +0700

    update new

--- src/pages/account/Subscribed.js
+++ src/pages/account/Subscribed.js
@@ -150,6 +150,193 @@ const Subscribed = () => {
             />
           ) : (
             <div className="w-full h-full xs:px-[20px] sm:px-[40px] md:px-[80px] lg:px-[120px] xl:px-[160px] 2xl:px-[200px] 3xl:px-[240px] bg-gray-100">
+              
+              {/* phần truyện series theo dõi */}
+              <div className="py-[10px] flex-row justify-center items-center container mx-auto my-auto">
+
+                <div className="m-2 flex justify-between mb-[30px]">
+
+                  <h1 className="flex items-center justify-center font-semibold text-lg text-black">
+                    {!language ?
+                      "ORIGINALS"
+                      :
+                      "오리지널"
+                    }
+                  </h1>
+
+                  {EditSubscribed ? (
+                    <div className="flex">
+                      <button
+                        className="w-[35px] max-h-[35px] font-semibold text-base text-black ml-3 px-2 rounded-full bg-[#dfdbdbec]"
+                        onClick={getALLSubscribed}
+                      >
+                        <CheckIcon
+                          sx={
+                            ALLSubcribed
+                              ? { color: "#31C48D" }
+                              : { color: "white" }
+                          }
+                        />
+                      </button>
+
+                      <button
+                        className="max-h-[35px] font-semibold text-base mr-2 ml-1 p-1 rounded-full text-gray-400"
+                        onClick={getALLSubscribed}
+                      >
+                        {!language ?
+                          "Select All"
+                          :
+                          "모두 선택"
+                        }
+                      </button>
+
+                      <button
+                        className="h-[35px] flex items-center justify-center font-semibold text-basg text-black border-gray-400 border py-2 px-7 rounded-full mr-5 ml-3"
+                        onClick={() => HandleDelete()}
+                      >
+                        {!language ?
+                          "Delete"
+                          :
+                          "삭제"
+                        }
+                      </button>
+                      <button
+                        className="h-[35px] flex items-center justify-center font-semibold text-basg text-white bg-gray-400 py-2 px-7 rounded-full"
+                        onClick={() => setEditSubscribed(!EditSubscribed)}
+                      >
+                        {!language ?
+                          "Cancel"
+                          :
+                          "취소"
+                        }
+                      </button>
+                    </div>
+                  ) : (
+                    <button
+                      className="h-[35px] flex items-center justify-center font-semibold text-basg text-white bg-gray-400 py-2 px-7 rounded-full"
+                      onClick={() => setEditSubscribed(!EditSubscribed)}
+                    >
+                      {!language ?
+                        "Edit"
+                        :
+                        "편집"
+                      }
+                    </button>
+                  )}
+                </div>
+
+                {EditSubscribed ? (
+                  <div className="w-full h-full grid grid-cols-5 gap-3 px-5">
+                    {Subscribed?.map((item) => {
+                      return (
+                        <div
+                          key={item?.id}
+                          className={`w-full h-full rounded-md ${checkSubcribed?.includes(item.id) ? "border-emerald-400" : ""}  relative`}
+                          onClick={() => getidSubscribed(item.id)}
+                        >
+                          <img
+                            src={item.squareThumbnail}
+                            alt="img"
+                            className="object-cover w-full h-full rounded-md"
+                          />
+                          <div className="absolute top-2 px-3">
+                            <span className=" text-shadow-white line-clamp-2 text-lg w-full font-bold">
+                              {item?.title}
+                            </span>
+                            <span className="w-full text-shadow-white line-clamp-1 text-md">
+                              {item?.Author}
+                            </span>
+                          </div>
+
+                          <div className="absolute top-[70%] px-3">
+                            <span className="w-full text-shadow-white line-clamp-1 text-base text-gray-500">
+                              {!language ?
+                                "Update"
+                                :
+                                "업데이트"
+                              }
+                            </span>
+                            {!language ?
+                              <span className="text-shadow-white line-clamp-1 text-base text-gray-500">
+                                {monthNames[new Date(item.createTime).getMonth()].en}{" "}
+                                {new Date(item.createTime).getDate()},
+                                {new Date(item.createTime)?.getFullYear()}
+                              </span>
+                              :
+                              <span className="text-shadow-white line-clamp-1 text-base text-gray-500">
+                                {monthNames[new Date(item.createTime).getMonth()].kr}{" "}
+                                {new Date(item.createTime).getDate()}일,
+                                {new Date(item.createTime)?.getFullYear()}년
+                              </span>
+                            }
+                          </div>
+
+                          <button className="absolute top-[75%] left-[75%] truncate line-clamp-5 text-base font-bold p-2 rounded-full bg-[#dfdbdbec]">
+                            <CheckIcon
+                              sx={
+                                checkSubcribed?.includes(item.id)
+                                  ? { color: "#31C48D" }
+                                  : { color: "white" }
+                              }
+                            />
+                          </button>
+                        </div>
+                      );
+                    })}
+                  </div>
+                ) : (
+                  <div className="w-full h-full grid grid-cols-5 gap-3 px-5">
+                    {Subscribed.map((item) => {
+                      return (
+                        <div
+                          key={item?.id}
+                          className="flex-row w-full h-full rounded-md relative overflow-hidden"
+                        >
+                          <img
+                            src={item.squareThumbnail}
+                            alt="img"
+                            className="object-cover w-full h-full rounded-md"
+                          />
+                          <div className="absolute top-2 px-3">
+                            <span className="w-full text-shadow-white line-clamp-2 text-lg font-bold">
+                              {item?.title}
+                            </span>
+                            <span className="w-full text-shadow-white line-clamp-1 text-md">
+                              {item?.Author}
+                            </span>
+                          </div>
+
+                          <div className="absolute top-[70%] px-3">
+                            <span className="w-full text-shadow-white line-clamp-1 text-base text-gray-500">
+                              {!language ?
+                                "Update"
+                                :
+                                "업데이트"
+                              }
+                            </span>
+
+                            {!language ?
+                              <span className="text-shadow-white line-clamp-1 text-base text-gray-500">
+                                {monthNames[new Date(item.createTime).getMonth()].en}{" "}
+                                {new Date(item.createTime).getDate()},
+                                {new Date(item.createTime)?.getFullYear()}
+                              </span>
+                              :
+                              <span className="text-shadow-white line-clamp-1 text-base text-gray-500">
+                                {monthNames[new Date(item.createTime).getMonth()].kr}{" "}
+                                {new Date(item.createTime).getDate()}일,
+                                {new Date(item.createTime)?.getFullYear()}년
+                              </span>}
+                          </div>
+
+                        </div>
+                      );
+                    })}
+                  </div>
+                )}
+              </div>
+
+              {/* phần video series theo dõi */}
               <div className="py-[10px] flex-row justify-center items-center container mx-auto my-auto">
 
                 <div className="m-2 flex justify-between mb-[30px]">
